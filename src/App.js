13861be33c0c3364d53f01afa2c9d2cdb46e657a
Julian <julian.widjaja02@gmail.com>
2022-12-02 17:37:27 -0800

    GW-27: Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import { MainContainer } from "./shared/layout/MainContainer";
 
 function App() {
   return (
     <div className="text-center text-2xl justify-center items-center h-screen">
-      Hello World
+      <MainContainer />
     </div>
   );
 }
---
+++ src/shared/layout/MainContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MainContainer = () => {
+  return (
+    <div>Main Container</div>
+  )
+}
