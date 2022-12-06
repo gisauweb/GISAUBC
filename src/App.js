3b7081459cbd2066598e7111442f396ba37b0e87
Julian <julian.widjaja02@gmail.com>
2022-12-05 19:02:41 -0800

    GW-27: Created the main container layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
 import { MainContainer } from "./shared/layout/MainContainer";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
     <div className="text-center text-2xl justify-center items-center h-screen">
-      <MainContainer />
+      <MainContainer>
+        <Typography variant="h4" gutterBottom className="text-red-500">
+          Main Container
+        </Typography>
+        <Typography>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </Typography>
+      </MainContainer>
     </div>
   );
 }
