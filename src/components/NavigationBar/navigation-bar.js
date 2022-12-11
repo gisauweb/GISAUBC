45f29be4b1c8288db7cde0c3fa68aa358c1c85f3
Christopher Wong <chrishadrianwong@gmail.com>
2022-12-10 18:30:36 -0800

    GW-21: Fixed linting errors

--- src/components/NavigationBar/navigation-bar.js
+++ src/components/NavigationBar/navigation-bar.js
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import { ReactComponent as NavLogo } from "../../assets/gisau_white.svg";
