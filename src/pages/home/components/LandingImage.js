ada4e63c0bcf9ab5d46289302e354022d11b8c5b
Julian <julian.widjaja02@gmail.com>
2022-12-22 00:34:12 -0800

    GW-20: Fix navigation bar & components position

--- src/pages/home/components/LandingImage.js
+++ src/pages/home/components/LandingImage.js
@@ -10,7 +10,7 @@ const useStyles = makeStyles({
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     backgroundPosition: "center",
-    // filter: "brightness(50%)",
+    filter: "brightness(50%)",
   },
 });
 
@@ -18,17 +18,17 @@ export const LandingImage = () => {
   const classes = useStyles();
 
   return (
-    <div
-      className={`landing-image h-screen ${classes.bgImage} flex justify-center items-center`}
-    >
-      <div className="">
-        <img src={Gisau} alt="Gisau Logo" className="w-3/10 m-auto"></img>
-        <h1 className="text-white my-4 font-proxima-nova">
-          Gado-Gado Indonesian Student Association <br />
-          of UBC
-        </h1>
-        <CustomButton />
-        <p className="text-white">Hello World</p>
+    <div className={`h-screen justify-center items-center`}>
+      <div className={`h-screen absolute w-full ${classes.bgImage}`}></div>
+      <div className="relative flex justify-center items-center h-full">
+        <div className="w-full mt-1/10">
+          <img src={Gisau} alt="Gisau Logo" className="w-1/5 m-auto"></img>
+          <h1 className="text-white my-8 font-proxima-nova text-center text-[2.5rem] font-semibold leading-12">
+            Gado-Gado Indonesian Student Association <br />
+            of UBC
+          </h1>
+          <CustomButton className="grid justify-center" />
+        </div>
       </div>
     </div>
   );
