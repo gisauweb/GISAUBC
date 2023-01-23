c7521cea7b51159def9f7fa1d3f349b5d3d2845f
Christopher Wong <chrishadrianwong@gmail.com>
2023-01-23 01:00:50 -0800

    GW-23: Fixed formatting

--- src/pages/home/components/activities/Activities.js
+++ src/pages/home/components/activities/Activities.js
@@ -28,9 +28,9 @@ const Activities = () => {
             <Box className="w-5/6">
               <img alt="Activity" id={activity.id} src={activity.image} className="h-full hover:cursor-pointer" onClick={handleClickActivity}/>
             </Box>
-            <Box className="w-5/6 text-center">
-              <Box className="font-proxima-nova text-2xl h-16" sx={{ my: 3, fontWeight: "bold"}}>{activity.title}</Box>
-              <Box className="font-proxima-nova text-lg px-2">{activity.description}</Box>
+            <Box className="w-3/4 text-center">
+              <Box className="font-proxima-nova text-2xl h-16" sx={{ my: 3, fontWeight: "bold", paddingRight: 1.5}}>{activity.title}</Box>
+              <Box className="font-proxima-nova text-lg pr-1">{activity.description}</Box>
             </Box>
           </Box>
         )}
