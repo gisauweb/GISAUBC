e6aec4c892f0cc4cd92c4011bc2af685a7fe0ebe
Christopher Wong <chrishadrianwong@gmail.com>
2023-01-25 23:15:47 -0800

    GW-25: Added background color to home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,16 +4,17 @@ import { MainContainer } from "../../shared/layout/MainContainer";
 import { LandingImage } from "./components/LandingImage";
 import Activities from "./components/Activities";
 import Partners from "./components/Partners";
+import { Box } from "@mui/material";
 
 export default function Home() {
   return (
-    <>
+    <Box className="bg-[#FFFDF5]">
       <LandingImage />
       <MainContainer>
         <About />
         <Activities />
         <Partners />
       </MainContainer>
-    </>
+    </Box>
   );
 }
