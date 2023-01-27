383ae71b8a9986105016ba2aba323c081f06b839
Julian <julian.widjaja02@gmail.com>
2023-01-26 16:21:33 -0800

    GW-24: Created the Event Layout for homepage

--- src/shared/layout/EventLayout.js
+++ src/shared/layout/EventLayout.js
@@ -1,34 +1,39 @@
 import React from "react";
 import { CustomButton } from "shared/components/CustomButton";
 
-import UpcomingEvents from "assets/events/upcoming_event.svg";
-import RumahHantu from "assets/events/rumah_hantu.jpg";
+import "./EventLayout.css";
 
-export const EventLayout = ({ className }) => {
+export const EventLayout = ({
+  className,
+  title,
+  events,
+  icon,
+  button1,
+  button2,
+}) => {
   return (
     <div className={className}>
-      <div className="uppercase flex gap-1/20">
-        <h1 className="title">Upcoming events</h1>
-        <img src={UpcomingEvents} alt="" className="max-w-[10%]"></img>
+      <div className="uppercase flex items-center">
+        <h1 className="title">{title}</h1>
+        <div className="events-icon">{icon}</div>
       </div>
-      <div className="mt-10 grid gap-y-8">
-        <div>
-          <p>
-            Come get your post-midterm scare at our Rumah Hantu! An event
-            collaboration with PERMIKA
-          </p>
-        </div>
-        <div>
-          <img
-            src={RumahHantu}
-            alt="Rumah Hantu"
-            className="rounded-3xl max-w-[95%]"
-          ></img>
-        </div>
-        <div className="grid justify-center gap-y-4">
-          <CustomButton />
-          <CustomButton />
-        </div>
+      <div className="mt-10">
+        {events.map((event, i) => (
+          <div className="grid gap-y-8">
+            <div>
+              <p>{event.desc}</p>
+            </div>
+            <div>
+              <div className="event-img">{event.img}</div>
+            </div>
+            <div className="grid justify-center">
+              <CustomButton name={button1} />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="grid justify-center mt-4">
+        <CustomButton name={button2.name} />
       </div>
     </div>
   );
