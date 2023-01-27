2a6447873f63d47a3b793722db86fb0252ee34cf
Julian <julian.widjaja02@gmail.com>
2023-01-26 16:22:33 -0800

    GW-24: Incorporated the event layout to homepage

--- src/pages/home/components/events/Events.js
+++ src/pages/home/components/events/Events.js
@@ -1,5 +1,27 @@
 import React from "react";
 
+import { EventLayout } from "shared/layout/EventLayout";
+import { UPCOMINGEVENTS, RANTANGAN } from "./events_data";
+
 export const Events = () => {
-  return <div>Events</div>;
+  return (
+    <div className="flex justify-between flex-wrap">
+      <EventLayout
+        className="basis-1/3"
+        title={UPCOMINGEVENTS.title}
+        events={UPCOMINGEVENTS.events}
+        icon={UPCOMINGEVENTS.icon}
+        button1={UPCOMINGEVENTS.button1}
+        button2={UPCOMINGEVENTS.button2}
+      />
+      <EventLayout
+        className="basis-1/3"
+        title={RANTANGAN.title}
+        events={RANTANGAN.events}
+        icon={RANTANGAN.icon}
+        button1={UPCOMINGEVENTS.button1}
+        button2={UPCOMINGEVENTS.button2}
+      />
+    </div>
+  );
 };
