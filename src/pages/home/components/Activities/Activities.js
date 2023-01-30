2463ad1a4bc17e81c283e572005595cf823351cf
Christopher Wong <chrishadrianwong@gmail.com>
2023-01-29 23:35:27 -0800

    GW-25: Modified activities

--- src/pages/home/components/Activities/Activities.js
+++ src/pages/home/components/Activities/Activities.js
@@ -28,7 +28,7 @@ const Activities = () => {
             <Box className="w-5/6">
               <img alt="Activity" id={activity.id} src={activity.image} className="h-full hover:cursor-pointer" onClick={handleClickActivity}/>
             </Box>
-            <Box className="w-5/6 text-center">
+            <Box className="w-5/6 text-center" sx={{pr: activity.padding}}>
               <Box className="font-proxima-nova text-2xl h-16" sx={{ my: 3, fontWeight: "bold"}}>{activity.title}</Box>
               <Box className="font-proxima-nova text-lg">{activity.description}</Box>
             </Box>
