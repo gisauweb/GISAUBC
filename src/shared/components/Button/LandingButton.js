c088d0c8df180da3cad79a688b982fd2dd6c2d2e
Julian <julian.widjaja02@gmail.com>
2023-02-02 00:13:53 -0800

    GW-28: Rename className in LandingButton

--- src/shared/components/Button/LandingButton.js
+++ src/shared/components/Button/LandingButton.js
@@ -2,13 +2,18 @@ import React from "react";
 
 import "./LandingButton.css";
 
-export const LandingButton = ({ className }) => {
+export const LandingButton = ({ className, text }) => {
+  const handleClick = () => {
+    console.log("Clicked");
+  };
+
   return (
     <div className={className}>
       <div
-        className={`button px-8 py-3 font-oswald normal-case font-normal text-lg`}
+        className={`landing-button px-8 py-3 font-oswald normal-case font-normal text-lg cursor-pointer`}
+        onClick={handleClick}
       >
-        Become a member!
+        {text}
       </div>
     </div>
   );
