e5246ad5f824bf8833cc720ba000a4b95c0a349f
Christopher Wong <chrishadrianwong@gmail.com>
2023-02-04 23:41:27 -0800

    GW-39: Updated responsiveness for desktop view

--- src/pages/home/components/About.js
+++ src/pages/home/components/About.js
@@ -5,7 +5,7 @@ import sunshine_image from "assets/sunshine.svg";
 import { useNavigate } from "react-router-dom";
 import HomeButton from "shared/components/HomeButton";
 
-const typographyStyle = "text-[18px] leading-tight font-proxima-nova my-5 w-full sm:pr-12";
+const typographyStyle = "text-[18px] md:text-[24px] leading-tight font-proxima-nova my-5 w-full sm:pr-12";
 
 const About = () => {
   const navigate = useNavigate();
@@ -17,16 +17,16 @@ const About = () => {
   return (
     <Box className="flex flex-col w-full text-center sm:text-left justify-center items-center sm:items-start pt-20">
       <Box className="w-1/2 flex pb-1.5">
-        <span className="text-xl sm:text-3xl font-bold font-oswald text-primary pt-8">
+        <span className="text-xl sm:text-3xl md:text-4xl font-bold font-oswald text-primary pt-4">
           WHO WE ARE
         </span>
-        <img src={sunshine_image} alt="Sunshine" className="h-10 sm:h-full relative right-4"></img>
+        <img src={sunshine_image} alt="Sunshine" className="h-10 sm:h-full relative right-4 bottom-4"></img>
       </Box>
-      <img
+      <a href="/about" className="sm:w-1/3 sm:absolute sm:right-1/10"><img
         src={about_image}
         alt="GISAU Executives"
-        className="sm:w-1/3 sm:absolute sm:right-1/10 md:w-1/4 md:right-[10%]"
-      />
+        className="w-full"
+      /></a>
       <Box className="w-1/2 h-full">
         <p className={typographyStyle}>
           Hello! We are a non-profit cultural organization centered in promoting
