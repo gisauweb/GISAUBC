319742c2e329b5f4040b79b9da58a5ce71f36e81
Christopher Wong <chrishadrianwong@gmail.com>
2023-02-05 00:28:26 -0800

    GW-39: Updated tailwind responsive syntaxes

--- src/pages/home/components/About.js
+++ src/pages/home/components/About.js
@@ -1,32 +1,37 @@
 import React from "react";
 import { Box } from "@mui/material";
-import about_image from "assets/about_2.png";
+import about_image from "assets/about/about.png";
+import about_mobile from "assets/about/about_mobile.svg";
 import sunshine_image from "assets/sunshine.svg";
 import { useNavigate } from "react-router-dom";
 import HomeButton from "shared/components/HomeButton";
+import { useMediaQuery } from "react-responsive";
 
-const typographyStyle = "text-[18px] md:text-[24px] leading-tight font-proxima-nova my-5 w-full sm:pr-12";
+const typographyStyle = "text-[10px] lg:text-[18px] xl:text-[24px] leading-tight font-proxima-nova my-5 w-full lg:pr-12";
 
 const About = () => {
   const navigate = useNavigate();
-
+  const isMobile = useMediaQuery({ query: `(max-width: 400) ` });
+  console.log("isMobile: ", isMobile)
   const handleClickButton = () => {
     navigate("about");
   };
 
   return (
-    <Box className="flex flex-col w-full text-center sm:text-left justify-center items-center sm:items-start pt-20">
+    <Box className="flex flex-col w-full text-center lg:text-left justify-center items-center lg:items-start pt-20">
       <Box className="w-1/2 flex pb-1.5">
-        <span className="text-xl sm:text-3xl md:text-4xl font-bold font-oswald text-primary pt-4">
+        <span className="text-xl lg:text-3xl xl:text-4xl font-bold font-oswald text-primary pt-4">
           WHO WE ARE
         </span>
-        <img src={sunshine_image} alt="Sunshine" className="h-10 sm:h-full relative right-4 bottom-4"></img>
+        <img src={sunshine_image} alt="Sunshine" className="h-10 lg:h-full relative right-4 bottom-4"></img>
       </Box>
-      <a href="/about" className="sm:w-1/3 sm:absolute sm:right-1/10"><img
-        src={about_image}
-        alt="GISAU Executives"
-        className="w-full"
-      /></a>
+      <a href="/about" className="lg:w-1/3 lg:absolute lg:right-1/10">
+        <img
+          src={isMobile ? about_mobile : about_image}
+          alt="GISAU Executives"
+          className="w-full"
+        />
+      </a>
       <Box className="w-1/2 h-full">
         <p className={typographyStyle}>
           Hello! We are a non-profit cultural organization centered in promoting
