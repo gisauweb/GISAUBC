853bfc3ef3566e63e1752ad1e38d1f043cf91b85
Julian <julian.widjaja02@gmail.com>
2023-02-08 00:43:36 -0800

    Gw-28: Add click handling in Button

--- src/shared/components/Button/Button.js
+++ src/shared/components/Button/Button.js
@@ -3,14 +3,21 @@ import { LandingButton } from "./LandingButton";
 
 import "./Button.css";
 
-export const Button = ({ landingButton, className, text, transparentBg }) => {
+export const Button = ({
+  landingButton,
+  className,
+  text,
+  handleClickButton,
+}) => {
   if (landingButton) {
     return <LandingButton className={className} text={text} />;
   }
 
   return (
     <div className={className}>
-      <div className={`button`}>{text}</div>
+      <div className={`button`} onClick={handleClickButton}>
+        {text}
+      </div>
     </div>
   );
 };
