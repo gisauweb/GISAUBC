429933f948ecb5a6bc12855b394b6a6f0e9ad515
Julian <julian.widjaja02@gmail.com>
2023-02-28 10:21:34 -0800

    GW-41: Upcoming events rantangan become responsive

--- src/pages/home/components/events/Events.js
+++ src/pages/home/components/events/Events.js
@@ -3,25 +3,45 @@ import React from "react";
 import { EventLayout } from "shared/layout/EventLayout";
 import { UPCOMINGEVENTS, RANTANGAN } from "./constants";
 
+import { useMediaQuery } from "react-responsive";
+
+const RESPONSIVE_SIZE = "1023px";
+
 export const Events = () => {
+  const isMobileView = useMediaQuery({
+    query: `(max-width: ${RESPONSIVE_SIZE})`,
+  });
+
+  let isRantanganShowed = true;
+
+  if (RANTANGAN.events.length === 0 && isMobileView) {
+    isRantanganShowed = false;
+  }
+
   return (
-    <div className="flex justify-between flex-wrap pt-24">
+    <div
+      className={`${!isMobileView && "flex"} justify-between flex-wrap pt-24`}
+    >
       <EventLayout
-        className="basis-1/3"
+        className={`${isRantanganShowed && "basis-1/3"} `}
         title={UPCOMINGEVENTS.title}
         events={UPCOMINGEVENTS.events}
         icon={UPCOMINGEVENTS.icon}
         button1={UPCOMINGEVENTS.button1}
         button2={UPCOMINGEVENTS.button2}
+        isMobileView={isMobileView}
       />
-      <EventLayout
-        className="basis-1/3"
-        title={RANTANGAN.title}
-        events={RANTANGAN.events}
-        icon={RANTANGAN.icon}
-        button1={RANTANGAN.button1}
-        button2={RANTANGAN.button2}
-      />
+      {isRantanganShowed && (
+        <EventLayout
+          className={`basis-1/3 ${isMobileView && "mt-24"}`}
+          title={RANTANGAN.title}
+          events={RANTANGAN.events}
+          icon={RANTANGAN.icon}
+          button1={RANTANGAN.button1}
+          button2={RANTANGAN.button2}
+          isMobileView={isMobileView}
+        />
+      )}
     </div>
   );
 };
