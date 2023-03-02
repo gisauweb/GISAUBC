57d8fde9e519f12f640caa4bd0db2273eb1bc12c
Christopher Wong <chrishadrianwong@gmail.com>
2023-03-01 20:27:05 -0800

    GW-42: Updated partners

--- src/pages/home/components/partners/Partners.js
+++ src/pages/home/components/partners/Partners.js
@@ -6,18 +6,18 @@ import { useMediaQuery } from 'react-responsive';
 
 const Partners = () => {
     const isLaptop = useMediaQuery({ query: `(max-width: 1440px)` });
-    const isTablet = useMediaQuery({ query: `(max-width: 1024px)` });
+    const isTablet = useMediaQuery({ query: `(max-width: 1023px)` });
     const isMobile = useMediaQuery({ query: `(max-width: 639px)` });	
     return (
         <Box className={`pt-24 ${isMobile && 'hidden'}`}>
-            <Box className="w-full flex pb-3 ">
+            <Box className="w-full flex pb-3 justify-center lg:justify-start">
                 <span className="text-3xl font-semibold font-oswald text-primary pt-8">
                     OUR PARTNERS
                 </span>
                 <img src={rainbow} alt="Rainbow" className="w-10 relative right-6 bottom-1.5"></img>
             </Box>
             <Box className='py-14'>
-                <Marquee gradient={false} speed={isTablet ? 35 : isLaptop ? 50 : 80}>
+                <Marquee gradient={false} speed={isTablet ? 50 : isLaptop ? 50 : 80}>
                     <div className="flex items-center justify-around flex-wrap">
                         {partners.map((partner, i) => (
                             <div className="mx-12" key={i}>
