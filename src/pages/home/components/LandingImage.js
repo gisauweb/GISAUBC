bcdac9b1a82af65a384932756cf564eacd33e4e3
Julian <julian.widjaja02@gmail.com>
2023-03-01 18:09:20 -0800

    GW-41: Incorporate feedbacks

--- src/pages/home/components/LandingImage.js
+++ src/pages/home/components/LandingImage.js
@@ -24,7 +24,7 @@ export const LandingImage = () => {
 
   return (
     <div className={`h-screen justify-center items-center overflow-hidden`}>
-      <div className={`h-full absolute w-full ${classes.bgImage}`}></div>
+      <div className={`h-screen absolute w-full ${classes.bgImage}`}></div>
       <div className="relative flex justify-center items-center h-full">
         <div className="w-full mt-1/10">
           <img
