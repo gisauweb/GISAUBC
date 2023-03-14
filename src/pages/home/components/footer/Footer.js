b0d9a477b801eba6433042842059a7e99050b703
Christopher Wong <chrishadrianwong@gmail.com>
2023-03-14 01:49:57 -0700

    GW-28: Updated footer responsiveness

--- src/pages/home/components/footer/Footer.js
+++ src/pages/home/components/footer/Footer.js
@@ -14,18 +14,18 @@ export const Footer = () => {
                 <img src={plane} alt="paper-plane"/>
             </div>
             <div className="footer mt-36 lg:mt-0">
-                <div className="footer-title text-lg lg:text-3xl">Connect with us!</div>
+                <div className="footer-title text-lg sm:text-2xl lg:text-3xl">Connect with us!</div>
                 <div className="footer-icons">
                     <a href='https://www.instagram.com/gisaubc/' className="icon"><img src={instagramIcon} alt="instagram-icon" /></a>
                     <a href='https://ca.linkedin.com/company/gisau' className="icon"><img src={linkedinIcon} alt="linkedin-icon" /></a>
                     <a href='https://www.facebook.com/gisaubc/' className="icon"><img src={facebookIcon} alt="facebook-icon" /></a>
                     <a href='https://open.spotify.com/show/4n3LXi2mKxLpscsIGVAgnR?si=rwRFIioeRnKj7irYurgZsQ&app_destination=copy-link' className="icon"><img src={spotifyIcon} alt="spotify-icon" /></a>
                 </div>
-                <div className="footer-email text-base lg:text-2xl">
+                <div className="footer-email text-base sm:text-xl lg:text-2xl">
                     <a href='mailto:contact.gisau@gmail.com'><Email className='mr-1' />
                         contact.gisau@gmail.com</a>
                 </div>
-                <div className="footer-copyright text-base lg:text-2xl">GISAUBC &#169; 2022</div>
+                <div className="footer-copyright text-base sm:text-xl lg:text-2xl">GISAUBC &#169; 2022</div>
             </div>
         </>
     )
