932363f63ced061ef37a5057d8c32aeb910736ee
Julian <julian.widjaja02@gmail.com>
2023-03-15 00:04:23 -0700

    GW-38: Modify navbar margin to be responsive

--- src/shared/components/navigation-bar/NavigationBar.js
+++ src/shared/components/navigation-bar/NavigationBar.js
@@ -9,20 +9,19 @@ import MenuIcon from "@mui/icons-material/Menu";
 import { MenuInterface } from "./MenuInterface";
 
 const NavigationBar = () => {
-  const isMobile = useMediaQuery({ query: `(max-width: 639px)` });
-  const isNavbarMenu = useMediaQuery({ query: `(max-width: 1023px)` });
+  const isMobile = useMediaQuery({ query: `(max-width: 767px)` });
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <div>
       <Box className="flex justify-between items-center absolute z-10 w-full top-1/20">
-        <Box className={isMobile ? `ml-6` : `ml-20`}>
+        <Box className={`ml-6 sm:ml-20 md:ml-6 lg:ml-20`}>
           <Link to="/">
-            <NavLogo className={`${isMobile ? `w-10` : "w-16"} h-auto`} />
+            <NavLogo className={`w-10 sm:w-16 h-auto`} />
           </Link>
         </Box>
-        {isNavbarMenu ? (
-          <div className={isMobile ? `mr-6` : `mr-20`}>
+        {isMobile ? (
+          <div className={`mr-6 sm:mr-20`}>
             {isMenuOpen ? (
               <MenuInterface
                 className={`w-6`}
@@ -36,7 +35,7 @@ const NavigationBar = () => {
             )}
           </div>
         ) : (
-          <Box className="flex mr-20">
+          <Box className="flex mr-6 lg:mr-20">
             {pages.map((page) => (
               <Link key={page.name} to={page.path} className="px-5 pt-3">
                 <p className="text-white hover:underline underline-offset-8 decoration-2 font-oswald text-xl">
