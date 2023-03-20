aec650f9f84e804beee3e2202730ad1f72e9dd77
Christopher Wong <chrishadrianwong@gmail.com>
2023-03-20 15:22:48 -0700

    GW-26: Added target="_blank" to icons

--- src/pages/home/components/footer/Footer.js
+++ src/pages/home/components/footer/Footer.js
@@ -19,10 +19,10 @@ export const Footer = () => {
             <div className="footer mt-36 lg:mt-0">
                 <div className="footer-title text-lg sm:text-2xl">Connect with us!</div>
                 <div className="footer-icons">
-                    <a href='https://www.instagram.com/gisaubc/' className="icon"><img src={instagramIcon} alt="instagram-icon" /></a>
-                    <a href='https://ca.linkedin.com/company/gisau' className="icon"><img src={linkedinIcon} alt="linkedin-icon" /></a>
-                    <a href='https://www.facebook.com/gisaubc/' className="icon"><img src={facebookIcon} alt="facebook-icon" /></a>
-                    <a href='https://open.spotify.com/show/4n3LXi2mKxLpscsIGVAgnR?si=rwRFIioeRnKj7irYurgZsQ&app_destination=copy-link' className="icon"><img src={spotifyIcon} alt="spotify-icon" /></a>
+                    <a href='https://www.instagram.com/gisaubc/' target="_blank" className="icon"><img src={instagramIcon} alt="instagram-icon" /></a>
+                    <a href='https://ca.linkedin.com/company/gisau' target="_blank" className="icon"><img src={linkedinIcon} alt="linkedin-icon" /></a>
+                    <a href='https://www.facebook.com/gisaubc/' target="_blank" className="icon"><img src={facebookIcon} alt="facebook-icon" /></a>
+                    <a href='https://open.spotify.com/show/4n3LXi2mKxLpscsIGVAgnR?si=rwRFIioeRnKj7irYurgZsQ&app_destination=copy-link' target="_blank" className="icon"><img src={spotifyIcon} alt="spotify-icon" /></a>
                 </div>
                 <div className="footer-email text-base sm:text-xl">
                     <a href='mailto:contact.gisau@gmail.com'><Email className='mr-1 pb-0.5' fontSize={isMobile ? 'small' : 'medium'}/>
