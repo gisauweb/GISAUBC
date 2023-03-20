7f235308709423c0dfccf32502e09a28beb803b0
Julian <julian.widjaja02@gmail.com>
2023-03-20 00:42:42 -0700

    GW-38: Create a simple transition when opening n closing the hamburger menu

--- src/shared/components/navigation-bar/MenuInterface.js
+++ src/shared/components/navigation-bar/MenuInterface.js
@@ -6,6 +6,48 @@ import CloseIcon from "@mui/icons-material/Close";
 import { ReactComponent as NavLogo } from "../../../assets/gisau-logo/gisau_white.svg";
 
 export const MenuInterface = ({ className, isOpen, setIsOpen, isHomePage }) => {
+  return (
+    <div className={className}>
+      {isOpen ? (
+        <CloseIcon
+          className="text-white relative z-50"
+          onClick={() => setIsOpen(false)}
+        />
+      ) : (
+        <MenuIcon
+          className={`${
+            isHomePage ? "text-white" : "text-black"
+          } relative z-50`}
+          onClick={() => setIsOpen(true)}
+        />
+      )}
+
+      <div
+        className={`bg-black fixed top-0 left-0 w-full h-full z-30   duration-500 ${
+          isOpen ? "block visible opacity-50" : "hidden invisible opacity-0"
+        }`}
+        onClick={() => setIsOpen(false)}
+      ></div>
+      <div
+        className={`fixed bg-primary top-0 right-0 w-[21rem] h-full z-40  duration-1000 ${
+          isOpen ? "translate-x-0 visible" : "invisible translate-x-full"
+        }`}
+      >
+        <div className="fixed right-0 mt-[4rem]">
+          <div className="text-right mr-6 mt-6 sm:mr-20">
+            {pages.map((page) => (
+              <Link key={page.name} to={page.path} className="px-5 pt-3">
+                <p className="text-white hover:underline underline-offset-8 decoration-2 font-oswald text-xl uppercase">
+                  {page.name}
+                </p>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   if (isOpen) {
     return (
       <div className={`${className}`}>
