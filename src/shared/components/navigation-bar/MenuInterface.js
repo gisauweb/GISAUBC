f2ba2764c8254d28ec926d261b51d0e826777108
Julian <julian.widjaja02@gmail.com>
2023-03-20 18:50:57 -0700

    GW-38: Add an animation to the hamburger icon

--- src/shared/components/navigation-bar/MenuInterface.js
+++ src/shared/components/navigation-bar/MenuInterface.js
@@ -8,20 +8,42 @@ import { ReactComponent as NavLogo } from "../../../assets/gisau-logo/gisau_whit
 export const MenuInterface = ({ className, isOpen, setIsOpen, isHomePage }) => {
   return (
     <div className={className}>
-      {isOpen ? (
-        <CloseIcon
-          className="text-white relative z-50"
-          onClick={() => setIsOpen(false)}
-        />
-      ) : (
-        <MenuIcon
-          className={`${
-            isHomePage ? "text-white" : "text-black"
-          } relative z-50`}
-          onClick={() => setIsOpen(true)}
-        />
-      )}
-
+      <div className="w-6 h-6 relative z-50" onClick={() => setIsOpen(!isOpen)}>
+        <div
+          className={`w-full h-full flex flex-col flex-wrap items-center content-center`}
+        >
+          <span
+            className={`menu-line-1 w-3 h-[2px] mt-[3px] mb-[3px] block rounded-l-sm duration-500 origin-[right_center] bg-white ${
+              isOpen && "translate-y-2 rotate-45"
+            }`}
+          ></span>
+          <span
+            className={`menu-line-2 w-3 h-[2px] mt-[3px] mb-[3px] block rounded-l-sm duration-500 origin-[right_center] bg-white ${
+              isOpen && "scale-0"
+            }`}
+          ></span>
+          <span
+            className={`menu-line-3 w-3 h-[2px] mt-[3px] mb-[3px] block rounded-l-sm duration-500 origin-[right_center] bg-white ${
+              isOpen && "-translate-y-2 -rotate-45"
+            }`}
+          ></span>
+          <span
+            className={`menu-line-4 w-3 h-[2px] mt-[3px] mb-[3px] block rounded-r-sm duration-500 origin-[left_center] bg-white ${
+              isOpen && "translate-y-2 -rotate-45"
+            }`}
+          ></span>
+          <span
+            className={`menu-line-5 w-3 h-[2px] mt-[3px] mb-[3px] block rounded-r-sm duration-500 origin-[left_center] bg-white ${
+              isOpen && "scale-0"
+            }`}
+          ></span>
+          <span
+            className={`menu-line-6 w-3 h-[2px] mt-[3px] mb-[3px] block rounded-r-sm duration-500 origin-[left_center] bg-white ${
+              isOpen && "-translate-y-2 rotate-45"
+            }`}
+          ></span>
+        </div>
+      </div>
       <div
         className={`bg-black fixed top-0 left-0 w-full h-full z-30   duration-500 ${
           isOpen ? "block visible opacity-50" : "hidden invisible opacity-0"
