ab97c281290cda3aeb681cb79a751fd2ca51f010
Julian <julian.widjaja02@gmail.com>
2023-03-23 01:04:01 -0700

    GW-51: Update the activities' images

--- src/pages/home/components/activities/Activities.js
+++ src/pages/home/components/activities/Activities.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { Box } from "@mui/material";
 import { activities, smiley } from "./constants";
-import { useNavigate } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 import { ActivitiesSlider } from "./Slider";
 
 const Activities = () => {
-  const navigate = useNavigate();
   const isMobile = useMediaQuery({ query: `(max-width: 1023px) ` });
-
-  const handleClickActivity = () => {
-    navigate("activities");
-  };
 
   return (
     <Box className="pt-24">
@@ -26,15 +20,40 @@ const Activities = () => {
         />
       </Box>
       <Box className="w-full flex lg:py-8">
-        {isMobile ? <ActivitiesSlider /> :
-          <>{activities.map((activity, index) =>
-            <Box key={index} className="w-1/3 flex flex-col" sx={{ alignItems: activity.alignment}}>
-              <Box className="w-5/6">
-                <img alt="Activity" id={activity.id} src={activity.image} className="h-full w-full hover:cursor-pointer" onClick={handleClickActivity} />
-              </Box>
-              <Box className="w-5/6 text-center px-3">
-                <Box className="font-proxima-nova text-2xl" sx={{ my: 3, fontWeight: "bold" }}>{activity.title}</Box>
-                <Box className="font-proxima-nova text-lg">{activity.description}</Box>
+        {isMobile ? (
+          <ActivitiesSlider className="mt-8" />
+        ) : (
+          <>
+            {activities.map((activity, index) => (
+              <Box
+                key={index}
+                className="w-1/3 flex flex-col"
+                sx={{ alignItems: activity.alignment }}
+              >
+                <Box className="w-5/6 relative">
+                  <img
+                    alt="Activity"
+                    id={activity.id}
+                    src={activity.image}
+                    className="h-full w-full hover:cursor-pointer"
+                  />
+                  <img
+                    alt="Icon"
+                    src={activity.icon}
+                    className={`absolute ${activity.iconStyle}`}
+                  />
+                </Box>
+                <Box className="w-5/6 text-center px-3">
+                  <Box
+                    className="font-proxima-nova text-2xl"
+                    sx={{ my: 3, fontWeight: "bold" }}
+                  >
+                    {activity.title}
+                  </Box>
+                  <Box className="font-proxima-nova text-lg">
+                    {activity.description}
+                  </Box>
+                </Box>
               </Box>
             </Box>
           )}</>
