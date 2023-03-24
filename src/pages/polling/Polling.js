0ab73a1858b38266d1b0aba1ac3ec15de6c39abe
Julian <julian.widjaja02@gmail.com>
2023-03-24 01:56:34 -0700

    GW-50: Add a switch to open and close the voting

--- src/pages/polling/Polling.js
+++ src/pages/polling/Polling.js
@@ -10,8 +10,10 @@ import {
 } from "@mui/material";
 import { writeVote } from "utils/firebase/polling";
 import PollingDialog from "./PollingDialog";
+import { LogoSpinning } from "shared/components/logo-spinning/LogoSpinning";
 
 function Polling() {
+  const isVotingOpen = process.env.VOTING_OPEN === "TRUE";
   const [president, setPresident] = useState("Abstain");
   const [vicePresident, setVicePresident] = useState("Abstain");
   const [treasurer, setTreasurer] = useState("Abstain");
@@ -45,110 +47,119 @@ function Polling() {
   return (
     <>
       <div className="min-h-screen bg-[#CFBBBB] flex flex-col items-center justify-center">
-        <div className="bg-white w-2/3 rounded-md p-6 flex flex-col items-center mt-40 mb-10">
-          <h1 className="text-center text-primary text-2xl md:text-3xl lg:text-4xl mb-8">
-            Gado-Gado Indonesian Students Association of UBC Elections
-          </h1>
-          <form onSubmit={handleSubmit} className="flex flex-col w-full">
-            <TextField
-              id="studentID"
-              label="Student ID"
-              type="text"
-              value={studentID}
-              onChange={(e) => setStudentID(e.target.value)}
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              sx={{ mt: 1, mb: 3 }}
-            />
-            <FormControl sx={{ mb: 3 }}>
-              <FormLabel sx={{ fontWeight: "700", color: "#7D0202" }}>
-                President
-              </FormLabel>
-              <RadioGroup
-                row
-                aria-label="president"
-                name="president"
-                value={president}
-                onChange={(e) => setPresident(e.target.value)}
+        {isVotingOpen ? (
+          <div className="bg-white w-2/3 rounded-md p-6 flex flex-col items-center mt-40 mb-10">
+            <h1 className="text-center text-primary text-2xl md:text-3xl lg:text-4xl mb-8">
+              Gado-Gado Indonesian Students Association of UBC Elections
+            </h1>
+            <form onSubmit={handleSubmit} className="flex flex-col w-full">
+              <TextField
+                id="studentID"
+                label="Student ID"
+                type="text"
+                value={studentID}
+                onChange={(e) => setStudentID(e.target.value)}
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                sx={{ mt: 1, mb: 3 }}
+              />
+              <FormControl sx={{ mb: 3 }}>
+                <FormLabel sx={{ fontWeight: "700", color: "#7D0202" }}>
+                  President
+                </FormLabel>
+                <RadioGroup
+                  row
+                  aria-label="president"
+                  name="president"
+                  value={president}
+                  onChange={(e) => setPresident(e.target.value)}
+                >
+                  <FormControlLabel
+                    value="Imelda Alimin"
+                    control={<Radio sx={{ color: "#7D0202" }} />}
+                    label="Imelda Alimin"
+                  />
+                  <FormControlLabel
+                    value="Jonathan Santoso"
+                    control={<Radio sx={{ color: "#7D0202" }} />}
+                    label="Jonathan Santoso"
+                  />
+                  <FormControlLabel
+                    value="Abstain"
+                    control={<Radio sx={{ color: "#7D0202" }} />}
+                    label="Abstain"
+                  />
+                </RadioGroup>
+              </FormControl>
+              <FormControl sx={{ mb: 3 }}>
+                <FormLabel sx={{ fontWeight: "700", color: "#7D0202" }}>
+                  Vice President
+                </FormLabel>
+                <RadioGroup
+                  row
+                  aria-label="vice-president"
+                  name="vice-president"
+                  value={vicePresident}
+                  onChange={(e) => setVicePresident(e.target.value)}
+                >
+                  <FormControlLabel
+                    value="Nadya Rei"
+                    control={<Radio sx={{ color: "#7D0202" }} />}
+                    label="Nadya Rei"
+                  />
+                  <FormControlLabel
+                    value="Abstain"
+                    control={<Radio sx={{ color: "#7D0202" }} />}
+                    label="Abstain"
+                  />
+                </RadioGroup>
+              </FormControl>
+              <FormControl sx={{ mb: 3 }}>
+                <FormLabel sx={{ fontWeight: "700", color: "#7D0202" }}>
+                  Treasurer
+                </FormLabel>
+                <RadioGroup
+                  row
+                  aria-label="treasurer"
+                  name="treasurer"
+                  value={treasurer}
+                  onChange={(e) => setTreasurer(e.target.value)}
+                >
+                  <FormControlLabel
+                    value="Joanico Huang"
+                    control={<Radio sx={{ color: "#7D0202" }} />}
+                    label="Joanico Huang"
+                  />
+                  <FormControlLabel
+                    value="Abstain"
+                    control={<Radio sx={{ color: "#7D0202" }} />}
+                    label="Abstain"
+                  />
+                </RadioGroup>
+              </FormControl>
+              <Button
+                type="submit"
+                variant="contained"
+                size="large"
+                style={{
+                  backgroundColor: "#7D0202",
+                  color: "#FFFFFF",
+                  marginTop: "1rem",
+                }}
               >
-                <FormControlLabel
-                  value="Imelda Alimin"
-                  control={<Radio sx={{ color: "#7D0202" }} />}
-                  label="Imelda Alimin"
-                />
-                <FormControlLabel
-                  value="Jonathan Santoso"
-                  control={<Radio sx={{ color: "#7D0202" }} />}
-                  label="Jonathan Santoso"
-                />
-                <FormControlLabel
-                  value="Abstain"
-                  control={<Radio sx={{ color: "#7D0202" }} />}
-                  label="Abstain"
-                />
-              </RadioGroup>
-            </FormControl>
-            <FormControl sx={{ mb: 3 }}>
-              <FormLabel sx={{ fontWeight: "700", color: "#7D0202" }}>
-                Vice President
-              </FormLabel>
-              <RadioGroup
-                row
-                aria-label="vice-president"
-                name="vice-president"
-                value={vicePresident}
-                onChange={(e) => setVicePresident(e.target.value)}
-              >
-                <FormControlLabel
-                  value="Nadya Rei"
-                  control={<Radio sx={{ color: "#7D0202" }} />}
-                  label="Nadya Rei"
-                />
-                <FormControlLabel
-                  value="Abstain"
-                  control={<Radio sx={{ color: "#7D0202" }} />}
-                  label="Abstain"
-                />
-              </RadioGroup>
-            </FormControl>
-            <FormControl sx={{ mb: 3 }}>
-              <FormLabel sx={{ fontWeight: "700", color: "#7D0202" }}>
-                Treasurer
-              </FormLabel>
-              <RadioGroup
-                row
-                aria-label="treasurer"
-                name="treasurer"
-                value={treasurer}
-                onChange={(e) => setTreasurer(e.target.value)}
-              >
-                <FormControlLabel
-                  value="Joanico Huang"
-                  control={<Radio sx={{ color: "#7D0202" }} />}
-                  label="Joanico Huang"
-                />
-                <FormControlLabel
-                  value="Abstain"
-                  control={<Radio sx={{ color: "#7D0202" }} />}
-                  label="Abstain"
-                />
-              </RadioGroup>
-            </FormControl>
-            <Button
-              type="submit"
-              variant="contained"
-              size="large"
-              style={{
-                backgroundColor: "#7D0202",
-                color: "#FFFFFF",
-                marginTop: "1rem",
-              }}
-            >
-              Submit
-            </Button>
-          </form>
-        </div>
+                Submit
+              </Button>
+            </form>
+          </div>
+        ) : (
+          <>
+            <LogoSpinning />
+            <h1 className="text-center text-primary text-2xl md:text-3xl lg:text-4xl mt-8">
+              Voting is now CLOSED!
+            </h1>
+          </>
+        )}
       </div>
       <PollingDialog
         loading={loading}
