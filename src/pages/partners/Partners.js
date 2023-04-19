8b99c65e5515c230aaa4841329b0605d417b996c
Christopher Wong <chrishadrianwong@gmail.com>
2023-04-18 19:35:50 -0700

    GW-47: Fixed linting issues

--- src/pages/partners/Partners.js
+++ src/pages/partners/Partners.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { MainContainer } from "../../shared/layout/MainContainer";
-import { Box } from "@mui/material";
+import { Box, Divider } from "@mui/material";
 import { Footer } from "../../shared/components/footer/Footer";
-import { SponsorDivider, SponsorRegistration, SponsorTitle, SponsorList } from "./components/index";
+import { SponsorRegistration, SponsorTitle, SponsorList } from "./components/index";
 import { ScrollButtonContainer } from "shared/components/ScrollButton";
-// import { LandingImage } from "pages/home/components/LandingImage";
 
 export const Partners = () => {
     return (
         <Box className="bg-[#FFFDF5]">
             <MainContainer>
                 <SponsorTitle />
-                <Divider color="#AF4328"/>
+                <Divider className="py-[0.5px]" color="black"/>
                 <SponsorList />
-                <Divider color="#AF4328"/>
+                <Divider className="py-[0.5px]" color="black"/>
                 <SponsorRegistration />
                 <ScrollButtonContainer threshold={1 / 4} />
             </MainContainer>
