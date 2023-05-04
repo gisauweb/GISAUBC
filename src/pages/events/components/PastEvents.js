526cf5bce93dee4fc3d3f69a01b21e92d3efd6fc
Christopher Wong <chrishadrianwong@gmail.com>
2023-05-04 10:29:51 -0700

    GW-57: Updated responsiveness for grid past events

--- src/pages/events/components/PastEvents.js
+++ src/pages/events/components/PastEvents.js
@@ -7,7 +7,7 @@ export const PastEvents = (props) => {
     const [selectedYear, setSelectedYear] = useState('2022/2023');
 
     const Item = ({ image, title, date, location }) => (
-        <Box className='flex flex-col'>
+        <Box className='flex flex-col mx-7 sm:mx-0'>
             <img src={image} alt="event_image" className='' />
             <Box className='text-center flex flex-col'>
                 <span className='py-2 pt-6 font-semibold text-xl'>{title}</span>
@@ -26,10 +26,10 @@ export const PastEvents = (props) => {
                 <DropdownMenu selectedYear={selectedYear} setSelectedYear={setSelectedYear} />
             </Box>
             <Box sx={{ flexGrow: 1, mt: 6 }}>
-                <Grid container spacing={{ xs: 2, md: 10 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                <Grid container spacing={{ xs: 10, md: 12 }} columns={{ xs: 2, sm: 8, md: 12 }}>
                     {PAST_EVENTS[selectedYear].map((event, index) => (
                         <Grid item xs={2} sm={4} md={4} key={index}>
-                            <Item image={event.image} title={event.title} date={event.date} location={event.location} />
+                            <Item image={event.image} title={event.title} date={event.date} location={event.loc} />
                         </Grid>
                     ))}
                 </Grid>
