50a747dff3258b02a8991a3d344bd822b595f1ed
Christopher Wong <chrishadrianwong@gmail.com>
2023-05-17 21:28:47 -0700

    GW-57: Implemented grow to grid container mobile view

--- src/shared/layout/GridContainer.js
+++ src/shared/layout/GridContainer.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import { Box, Grid } from '@mui/material'
+import React, { useState } from 'react'
+import { Box, Grid, Grow } from '@mui/material'
 import { Button } from 'shared/components/button/Button'
+import { useMediaQuery } from 'react-responsive';
 
 const GridContainer = ({ data, source, button, className }) => {
+    const dataLength = data.length
+    const [grow, setGrow] = useState(false)
+    const isMobile = useMediaQuery({ query: `(max-width: 639px) ` });
+
     const handleClickButton = (link) => {
-        window.open(link, "_blank", "noreferrer");
+        window.open("https://" + link, "_blank", "noreferrer");
     };
 
+    const handleChange = () => {
+        setGrow(!grow);
+    };
+
+
     const GridItem = ({ event }) => (
-        <Grid item xs={2} sm={4} md={4}>
+        <Grid item xs={1} sm={2} md={2}>
             <Box className='flex flex-col mx-7 sm:mx-0'>
                 <img src={event.image} alt="event_image" className='' />
                 {source === 'events' &&
@@ -32,10 +42,45 @@ const GridContainer = ({ data, source, button, className }) => {
 
     return (
         <Box sx={{ flexGrow: 1, mt: 6 }} className={className}>
-            <Grid container spacing={{ xs: 10, md: 12 }} columns={{ xs: 2, sm: 8, md: 12 }}>
-                {data.map((event, index) => (
+            <Grid container spacing={{ xs: 10, md: 12 }} columns={{ xs: 1, sm: 4, md: 6 }}>
+                {data.slice(0, isMobile ? dataLength / 3 : dataLength).map((event, index) => (
                     <GridItem event={event} key={index} />
                 ))}
+                {grow ? <>
+                    <Grow in={grow}
+                        style={{ transformOrigin: '0 0 0', width: "100%" }}
+                        {...(grow ? { timeout: 1500 } : {})}
+                    >
+                        <Grid item xs={1} sm={2} md={2}>
+                            <Grid container spacing={{ xs: 10, md: 12 }} columns={{ xs: 1, sm: 4, md: 6 }}>
+                                {data.slice(dataLength / 3, dataLength * 2 / 3).map((event, index) => (
+                                    <GridItem event={event} key={index} />
+                                ))}
+                            </Grid>
+                        </Grid>
+                    </Grow>
+                    {/* Conditionally applies the timeout prop to change the entry speed. */}
+                    <Grow
+                        in={grow}
+                        style={{ transformOrigin: '0 0 0', width: "100%" }}
+                        {...(grow ? { timeout: 3000 } : {})}
+                    >
+                        <Grid item xs={1} sm={2} md={2}>
+                            <Grid container spacing={{ xs: 10, md: 12 }} columns={{ xs: 1, sm: 4, md: 6 }}>
+                                {data.slice(dataLength * 2 / 3).map((event, index) => (
+                                    <GridItem event={event} key={index} />
+                                ))}
+                            </Grid>
+                        </Grid>
+                    </Grow>
+                    <Grid item xs={1} sm={2} md={2} className='flex justify-center'>
+                        <Button text="Show Less" handleClickButton={handleChange} transparentBg={true} />
+                    </Grid>
+                </> : isMobile &&
+                <Grid item xs={1} sm={2} md={2} className='flex justify-center'>
+                    <Button text="Show More" handleClickButton={handleChange} />
+                </Grid>
+                }
             </Grid>
         </Box>
     )
