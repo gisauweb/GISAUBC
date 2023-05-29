d1c7f609b7defaa333ff024c71309d0fe5d01bfc
Christopher Wong <chrishadrianwong@gmail.com>
2023-05-29 14:56:46 -0700

    GW-61: Removed MUI Styles from package.json

--- src/shared/components/landing-image/LandingImage.js
+++ src/shared/components/landing-image/LandingImage.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 export const LandingImage = ({ bgImage, children }) => {
-  const background = bgImage();
+  const backgroundStyles = {
+    filter: "brightness(0.35)",
+    backgroundSize: "cover",
+    backgroundImage: `url(${bgImage})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+  };
 
   return (
     <div className={`h-screen items-center overflow-hidden`}>
-      <div className={`absolute w-full h-full ${background.styles}`} />
+      <div className={`absolute w-full h-full`} style={backgroundStyles} />
       <div className="relative flex justify-center items-center h-full">
         <div className="w-full mt-1/10">{children}</div>
       </div>
