6a6f005ba485f0f00b9522005313b2d46fc9694f
Christopher Wong <chrishadrianwong@gmail.com>
2023-05-29 17:17:02 -0700

    GW-58: Refactored partners page

--- src/pages/partners/Partners.js
+++ src/pages/partners/Partners.js
@@ -4,11 +4,13 @@ import { Box, Divider } from "@mui/material";
 import { Footer } from "../../shared/components/footer/Footer";
 import { SponsorRegistration, SponsorTitle, SponsorList } from "./components/index";
 import { ScrollButtonContainer } from "shared/components/ScrollButton";
+import { LandingImage } from "shared/components/landing-image/LandingImage";
+import landing_img from "assets/landing/partners.jpeg"
 
 export const Partners = () => {
     return (
         <Box className="bg-[#FFFDF5]">
-            <div className="flex justify-center"/>
+            <LandingImage bgImage={landing_img} text={"Our Partners"} />
             <MainContainer>
                 <SponsorTitle />
                 <Divider className="py-[0.5px]" color="black"/>
--- src/pages/partners/components/SponsorContainer.js
+++ src/pages/partners/components/SponsorContainer.js
@@ -16,11 +16,11 @@ const SponsorContainer = ({ name, logo, reversed }) => {
     }
 
     return (
-        <Container style={containerStyle} className="py-3">
+        <Container style={containerStyle} className="py-6">
             <Box style={boxStyle} className="justify-end pr-4 sm:pr-9">
                 {
                     reversed ?
-                        <span className="text-2xl sm:text-3xl xl:text-4xl font-semibold font-oswald text-primary pt-4 lg:pt-6 text-right">
+                        <span className="text-2xl sm:text-3xl xl:text-4xl font-semibold font-oswald text-primary text-right">
                             {name}
                         </span>
                         : <img alt={name} src={logo} className="w-3/4 h-auto rounded-md sm:w-3/5 lg:w-2/5 sm:max-h-60" />
@@ -30,7 +30,7 @@ const SponsorContainer = ({ name, logo, reversed }) => {
                 {
                     reversed ?
                         <img alt={name} src={logo} className="w-3/4 h-auto rounded-md sm:w-2/5 sm:max-h-60" />
-                        : <span className="text-2xl sm:text-3xl xl:text-4xl font-semibold font-oswald text-primary pt-4 lg:pt-6 text-left">
+                        : <span className="text-2xl sm:text-3xl xl:text-4xl font-semibold font-oswald text-primary text-left">
                             {name}
                         </span>
                 }
