cc72a717232dc7c75cd43874fe470b98be609f36
Christopher Wong <chrishadrianwong@gmail.com>
2023-06-01 18:06:48 -0700

    GW-66: Add become an executive in landing image

--- src/pages/home/components/HomeLandingImage.js
+++ src/pages/home/components/HomeLandingImage.js
@@ -11,28 +11,29 @@ export const HomeLandingImage = () => {
   });
 
   const handleClickButton = () => {
-    window.open("https://forms.gle/qujebG19m2VJzvBB6", "_blank", "noreferrer");
+    window.open("https://drive.google.com/file/d/1pznE1l3oLW-n5KpCiyQ6UWNShFJgJ-fQ/view?usp=drive_link", "_blank", "noreferrer");
   };
 
   return (
-      <LandingImage bgImage={home_landing_img} isHomePage={true}>
-        <img
-          src={gisau_logo}
-          alt="Gisau Logo"
-          className="w-[30%] md:w-1/4 lg:w-1/5 m-auto"
+    <LandingImage bgImage={home_landing_img} isHomePage={true}>
+      <img
+        src={gisau_logo}
+        alt="Gisau Logo"
+        className="w-[30%] md:w-1/4 lg:w-1/5 m-auto"
+      />
+      <h1 className="text-white my-4 md:my-8 mx-2 font-montserrat text-center font-semibold text-xl sm:text-2xl md:text-4xl lg:text-[2.5rem] lg:leading-12">
+        Gado-Gado Indonesian Student Association {!isMobileView && <br />} of
+        UBC
+      </h1>
+      <div className="flex gap-x-4 justify-center">
+        <Button
+          className="grid justify-center"
+          text="Become an executive!"
+          landingButton={true}
+          // transparentBg={true}
+          handleClickButton={handleClickButton}
         />
-        <h1 className="text-white my-4 md:my-8 mx-2 font-montserrat text-center font-semibold text-xl sm:text-2xl md:text-4xl lg:text-[2.5rem] lg:leading-12">
-          Gado-Gado Indonesian Student Association {!isMobileView && <br />} of
-          UBC
-        </h1>
-        <div className="flex gap-x-4 justify-center">
-          <Button
-            className="grid justify-center"
-            text="Become a member!"
-            landingButton={true}
-            handleClickButton={handleClickButton}
-          />
-        </div>
-      </LandingImage>
+      </div>
+    </LandingImage>
   );
 };
