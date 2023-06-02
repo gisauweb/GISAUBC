d510442cc1a5275f196f2f3caaa6246356d7343e
Christopher Wong <chrishadrianwong@gmail.com>
2023-06-01 19:42:31 -0700

    Fixed responsive issue

--- src/shared/components/landing-image/LandingImage.js
+++ src/shared/components/landing-image/LandingImage.js
@@ -12,7 +12,7 @@ export const LandingImage = ({ bgImage, text, isHomePage, children }) => {
   return (
     <div className={`h-screen items-center overflow-hidden`}>
       <div className={`absolute w-full h-[100svh]`} style={backgroundStyles} />
-      <div className="relative flex justify-center items-center h-full">
+      <div className="relative flex justify-center items-center h-[100svh]">
         <div className="w-full mt-1/10">
           {isHomePage ? children :
             <h1 className="text-white my-4 md:my-8 mx-2 font-montserrat text-center font-semibold text-3xl sm:text-5xl lg:text-7xl uppercase tracking-wide">
