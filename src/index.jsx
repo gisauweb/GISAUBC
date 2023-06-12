95961d78e8514c95655b0a388aa865f607761490
Christopher Wong <chrishadrianwong@gmail.com>
2023-06-12 00:08:48 -0700

    GW-63: GA fix

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { initializeGtag } from 'gtag-ga';
+// import { initializeGtag } from 'gtag-ga';
+import { gtag, install } from 'ga-gtag';
 import App from './App';
 
-initializeGtag(process.env.REACT_APP_GID);
+// initializeGtag(process.env.REACT_APP_GID);
+
+install(process.env.REACT_APP_GID);
+gtag('event', 'start');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
