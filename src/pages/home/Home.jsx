e550bcec0c80ad222962cd6494fb0355316ef9d7
Christopher Wong <chrishadrianwong@gmail.com>
2023-06-14 01:06:50 -0700

    GW-63: Removed pageview since it's already set by defualt

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Box } from '@mui/material';
-import ReactGA from 'react-ga4';
-import { useLocation } from 'react-router-dom';
 import About from './components/About';
 import MainContainer from '../../shared/layout/MainContainer';
 import HomeLandingImg from './components/HomeLandingImage';
@@ -12,11 +10,6 @@ import Footer from '../../shared/components/footer/Footer';
 import ScrollButtonContainer from '../../shared/components/ScrollButton';
 
 export default function Home() {
-	const location = useLocation();
-	useEffect(() => {
-		ReactGA.send({ hitType: 'pageview', page: location.pathname });
-	}, [location]);
-
 	return (
 		<Box className='bg-[#FFFDF5]'>
 			<HomeLandingImg />
