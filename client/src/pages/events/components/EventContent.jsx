a50d054599d371b868edebf7f16751438c16361c
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2023-06-19 00:04:09 -0700

    GW-63: Initial Express Setup (#85)
    * GW-63: Moved react app to client folder
    
    * GW-63: Initialized routing controllers express app
    
    * GW-63: Removed node_modules from git
    
    * GW-63: Initialized routing-controller typescript express
    
    * GW-63: Updated express formatting
    
    * GW-63: Remove error temporarily
    
    * GW-63: Fixed configs errors
    
    * GW-63: Fixed tailwind config problem
    
    * GW-63: Created index for shared components
    
    * GW-63: Fixed home-landing-image issue
    
    * GW-63: Resolved PR comments
    
    * GW-63: Fixed build error
    
    * GW-63: Fixed build issue

---
+++ client/src/pages/events/components/EventContent.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+import GridContainer from 'shared/layout/GridContainer';
+import { Typography, DropdownMenu } from 'shared/components/index';
+import UPCOMING_EVENTS from 'shared/data/upcoming_event';
+import PAST_EVENTS from './constants';
+
+function EventContent({ upcoming }) {
+	const [selectedYear, setSelectedYear] = useState('2022/2023');
+	const features = {
+		event: true,
+		upcomingEvent: upcoming,
+	};
+
+	return (
+		<Box className='my-20'>
+			<Box className={`flex w-full justify-between ${!upcoming && 'mb-36 sm:mb-0'}`}>
+				<Typography variant='h2' text={`${upcoming ? 'UPCOMING' : 'PAST'} EVENTS`} className='pt-0.5' />
+				{!upcoming && <DropdownMenu selectedYear={selectedYear} setSelectedYear={setSelectedYear} />}
+			</Box>
+			<GridContainer
+				data={upcoming ? UPCOMING_EVENTS : PAST_EVENTS[selectedYear]}
+				features={features}
+				className='sm:my-28'
+			/>
+		</Box>
+	);
+}
+
+export default EventContent;
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import MainContainer from 'shared/layout/MainContainer';
+import { Footer, ScrollButton } from 'shared/components/index';
+import About from './components/About';
+import HomeLandingImg from './components/HomeLandingImage';
+import Activities from './components/activities/Activities';
+import Events from './components/events/Events';
+import Partners from './components/partners/Partners';
+
+export default function Home() {
+	return (
+		<Box className='bg-[#FFFDF5]'>
+			<HomeLandingImg />
+			<MainContainer>
+				<About />
+				<Activities />
+				<Events />
+				<Partners />
+				<ScrollButton threshold={13 / 20} />
+			</MainContainer>
+			<Footer showPlane />
+		</Box>
+	);
+}
