9ddf87e9931cae5ffaa26f5d189cab77cb42f092
Christopher Wong <chrishadrianwong@gmail.com>
2023-06-27 00:59:38 -0700

    GW-71: Added executives pics

---
+++ client/src/assets/about/executives/index.js
@@ -0,0 +1,8 @@
+import img1 from './22:23/christo.jpeg';
+import img2 from './22:23/russell.jpeg';
+import img3 from './22:23/ella.jpeg';
+import img4 from './22:23/imel.jpeg';
+
+const EXECS_22_23 = [img1, img2, img3, img4];
+
+export default EXECS_22_23;
