7ef7f1c62bbf1aa8b575da5c2651ff4e9c2000f4
Christopher Wong <chrishadrianwong@gmail.com>
2023-06-27 01:01:05 -0700

    GW-71: Added constants and refactored teambuttons

--- client/src/pages/about/TeamButtons.jsx
+++ client/src/pages/about/TeamButtons.jsx
@@ -1,22 +1,20 @@
 import { Box } from '@mui/material';
-import React, { useState } from 'react';
+import React from 'react';
 import { Button } from 'shared/components';
+import { BUTTONS } from './constants';
 
-export default function TeamButtons() {
-	const [selected, setSelected] = useState('All');
-	const buttons = ['All', 'Core', 'Events', 'Marketing', 'FnB'];
-
+export default function TeamButtons({ selectedButton, setSelectedButton }) {
 	const handleClickButton = (buttonName) => {
-		setSelected(buttonName);
+		setSelectedButton(buttonName);
 	};
 
 	return (
 		<Box className='flex space-x-10'>
-			{buttons.map((buttonName) => (
+			{BUTTONS.map((buttonName) => (
 				<Button
 					key={buttonName}
 					text={buttonName}
-					background={selected === buttonName ? '' : 'grayBg'}
+					background={selectedButton === buttonName ? '' : 'grayBg'}
 					handleClickButton={() => {
 						handleClickButton(buttonName);
 					}}
---
+++ client/src/pages/about/constants.js
@@ -0,0 +1,46 @@
+import EXECS_22_23 from 'assets/about/executives';
+
+const BUTTONS = ['All', 'Core', 'Events', 'Marketing', 'FnB'];
+
+const TEAM_MEMBERS = {
+	'2022/2023': [
+		{
+			image: EXECS_22_23[0],
+			name: 'Christopher Wong',
+			education: '4th Year Computer Science',
+			link: 'www.linkedin.com/in/chrishdwong',
+		},
+		{
+			image: EXECS_22_23[1],
+			name: 'Christopher Wong',
+			education: '4th Year Computer Science',
+			link: 'www.linkedin.com/in/chrishdwong',
+		},
+		{
+			image: EXECS_22_23[2],
+			name: 'Christopher Wong',
+			education: '4th Year Computer Science',
+			link: 'www.linkedin.com/in/chrishdwong',
+		},
+		{
+			image: EXECS_22_23[2],
+			name: 'Christopher Wong',
+			education: '4th Year Computer Science',
+			link: 'www.linkedin.com/in/chrishdwong',
+		},
+		{
+			image: EXECS_22_23[0],
+			name: 'Christopher Wong',
+			education: '4th Year Computer Science',
+			link: 'www.linkedin.com/in/chrishdwong',
+		},
+		{
+			image: EXECS_22_23[1],
+			name: 'Christopher Wong',
+			education: '4th Year Computer Science',
+			link: 'www.linkedin.com/in/chrishdwong',
+		},
+	],
+};
+
+export { BUTTONS, TEAM_MEMBERS };
