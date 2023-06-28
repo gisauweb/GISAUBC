007f1f7876ee5cf9782f79e95c3ef693a40687b2
Christopher Wong <chrishadrianwong@gmail.com>
2023-06-28 16:23:31 -0700

    GW-71: Added execs pics

--- client/src/assets/about/executives/index.js
+++ client/src/assets/about/executives/index.js
@@ -1,8 +1,57 @@
-import img1 from './22:23/christo.jpeg';
-import img2 from './22:23/russell.jpeg';
-import img3 from './22:23/ella.jpeg';
-import img4 from './22:23/imel.jpeg';
+import img1 from './22:23/christie.jpg';
+import img2 from './22:23/christo.jpg';
+import img3 from './22:23/cia.jpg';
+import img4 from './22:23/ella.jpg';
+import img5 from './22:23/findy.jpg';
+import img6 from './22:23/ian.jpg';
+import img7 from './22:23/imel.jpg';
+import img8 from './22:23/jade.jpg';
+import img9 from './22:23/jena.jpg';
+import img10 from './22:23/josan.jpg';
+import img11 from './22:23/julian.jpg';
+import img12 from './22:23/kelly.jpg';
+import img13 from './22:23/kent.jpg';
+import img14 from './22:23/luna.jpg';
+import img15 from './22:23/mj.jpg';
+import img16 from './22:23/nadya.jpg';
+import img17 from './22:23/nando.jpg';
+import img18 from './22:23/nico.jpg';
+import img19 from './22:23/rachel.jpg';
+import img20 from './22:23/richelle.jpg';
+import img21 from './22:23/russell.jpg';
+import img22 from './22:23/sasha.jpg';
+import img23 from './22:23/sidney.jpg';
+import img24 from './22:23/val.jpg';
+import img25 from './22:23/vincent.jpg';
+import img26 from './22:23/vinny.jpg';
 
-const EXECS_22_23 = [img1, img2, img3, img4];
+const EXECS_22_23 = [
+	img1,
+	img2,
+	img3,
+	img4,
+	img5,
+	img6,
+	img7,
+	img8,
+	img9,
+	img10,
+	img11,
+	img12,
+	img13,
+	img14,
+	img15,
+	img16,
+	img17,
+	img18,
+	img19,
+	img20,
+	img21,
+	img22,
+	img23,
+	img24,
+	img25,
+	img26,
+];
 
 export default EXECS_22_23;
