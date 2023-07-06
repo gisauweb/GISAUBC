d125d936b9ed011c672cb584c62400ec3ee4b28e
Christopher Wong <chrishadrianwong@gmail.com>
2023-07-06 15:37:53 -0700

    GW-71: Updated responsiveness

--- client/src/pages/about/components/ExpandableBox.jsx
+++ client/src/pages/about/components/ExpandableBox.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+import { useMediaQuery } from 'react-responsive';
 
 function ExpandableBox({ title, description, expandedBox, setExpandedBox }) {
 	const expanded = expandedBox === title;
+	const isMobileView = useMediaQuery({ query: '(max-width: 639px)' });
 
 	const handleClick = () => {
 		setExpandedBox((prev) => (prev === title ? null : title));
@@ -10,38 +12,37 @@ function ExpandableBox({ title, description, expandedBox, setExpandedBox }) {
 
 	return (
 		<Box
+			className={`flex items-center rounded-md cursor-pointer bg-bgPrimary ${expanded ? 'h-56 sm:h-40' : 'h-16'}`}
 			sx={{
-				display: 'flex',
-				alignItems: 'center',
-				height: expanded ? '10rem' : '5rem',
 				borderBottom: '1px solid #e2e8f0',
-				borderRadius: '6px',
 				transition: 'box-shadow 0.3s ease-in-out',
 				boxShadow: expanded ? '0px 3px 5px rgba(0, 0, 0, 0.4)' : '0px 1px 2px rgba(0, 0, 0, 0.1)',
 				'&:hover': {
 					transform: 'translateY(-2px)',
 					boxShadow: '0px 5px 10px rgba(0, 0, 0, 0.4)',
 				},
-				cursor: 'pointer',
-				backgroundColor: '#FFFDF5',
 			}}
 			onClick={handleClick}
 		>
 			<Box
+				className='rounded-md'
 				sx={{
-					width: expanded ? '1.2rem' : '0.38rem',
+					width: '1px',
 					height: '100%',
 					backgroundColor: expanded ? '#7D0202' : '#B31717',
-					borderRadius: '6px',
 					transition: 'background-color 1s ease-in-out',
-					mr: 1,
+					pr: 0.5,
 				}}
 			/>
-			<Box className='flex flex-col p-2 '>
-				<Typography variant='h6' sx={{ fontWeight: 'bold' }}>
+			<Box className='flex flex-col pl-4'>
+				<Typography variant={isMobileView ? 'subtitle2' : 'h6'} sx={{ fontWeight: 'bold' }}>
 					{title}
 				</Typography>
-				{expanded && <Typography variant='body1'>{description}</Typography>}
+				{expanded && (
+					<Typography variant={isMobileView ? 'caption' : 'body1'} sx={{ mt: 1 }}>
+						{description}
+					</Typography>
+				)}
 			</Box>
 		</Box>
 	);
--- client/src/pages/about/components/Principles.jsx
+++ client/src/pages/about/components/Principles.jsx
@@ -8,27 +8,24 @@ export default function Principles() {
 
 	return (
 		<Box
-			className='flex my-12'
+			className='flex flex-col items-center sm:flex-row sm:items-left my-12 p-8 bg-[#FEF8EF] rounded-xl'
 			sx={{
 				gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
-				padding: '2rem',
-				backgroundColor: '#FEF8EF',
-				borderRadius: '12px',
 				boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
-				height: '25rem',
 			}}
 		>
-			<Box className='w-1/2 flex flex-col justify-center items-center'>
-				<span className='text-7xl text-primary'>Our</span>
-				<span className='text-7xl text-primary'>Values</span>
+			<Box className='w-full sm:w-1/2 flex flex-col justify-center items-center mb-6 sm:mb-0'>
+				<span className='text-4xl sm:text-7xl text-primary'>Our</span>
+				<span className='text-4xl sm:text-7xl text-primary'>Values</span>
 			</Box>
-			<Box className='flex flex-col w-1/2 justify-center space-y-4'>
+			<Box className='flex flex-col w-full sm:w-1/2 justify-center space-y-4'>
 				{VISION_MISSION.map((item) => (
 					<ExpandableBox
 						title={item.vision}
 						description={item.mission}
 						expandedBox={expandedBox}
 						setExpandedBox={setExpandedBox}
+						key={item.vision}
 					/>
 				))}
 			</Box>
