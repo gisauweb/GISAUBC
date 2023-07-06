d7506e7a3b54a73dbdab83abf7f0a0d01d88d56b
Christopher Wong <chrishadrianwong@gmail.com>
2023-07-06 00:40:55 -0700

    GW-71: Updated higlights

--- client/src/pages/about/components/Highlights.jsx
+++ client/src/pages/about/components/Highlights.jsx
@@ -12,7 +12,7 @@ export default function Highlights() {
 				<div className='flex items-center justify-around flex-wrap'>
 					{HIGHLIGHTS.map((image) => (
 						<div className='mx-5' key={image}>
-							<img src={image} alt='higlight' className='h-80 aspect-[1.5] object-fill' />
+							<img src={image} alt='higlight' className='h-80' />
 						</div>
 					))}
 				</div>
