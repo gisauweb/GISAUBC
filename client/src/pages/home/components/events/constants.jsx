b297f471439600c8716f77ca5fecef1ed0929469
Christopher Wong <chrishadrianwong@gmail.com>
2023-07-23 21:46:34 -0700

    Added ubud to upcoming event

--- client/src/pages/home/components/events/constants.jsx
+++ client/src/pages/home/components/events/constants.jsx
@@ -1,5 +1,6 @@
 import rantangan from 'assets/home-page/events/rantangan.svg';
 import upcomingEvent from 'assets/home-page/events/upcoming_event.svg';
+import ubud from 'assets/home-page/events/ubud.jpeg';
 
 /**
  * events field:
@@ -10,7 +11,14 @@ import upcomingEvent from 'assets/home-page/events/upcoming_event.svg';
 
 export const UPCOMINGEVENTS = {
 	title: 'Upcoming Events',
-	events: [],
+	events: [
+		{
+			title: 'UBUD: UBC Udah Dekat',
+			desc: ['Friday, August 4 | 8-9:40 PM', 'Online on Zoom'],
+			img: <img src={ubud} alt='UBUD: UBC Udah Dekat' />,
+			link: 'forms.gle/AXqcbDGtKBBiHCqX6',
+		},
+	],
 	icon: <img src={upcomingEvent} alt='' className='ml-[25%] w-[80%] sm:ml-[35%] sm:w-full' />,
 	button1: 'Register',
 	button2: {
