5d896c033ea9ba687c8dee3431c36910b2c1958e
Flayre <mariesamantha.f@gmail.com>
2023-07-26 12:56:20 -0700

    GW-72: Added rantangan folder to directory, test commit

---
+++ client/src/pages/rantangan/Rantangan.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import MainContainer from 'shared/layout/MainContainer';
+import { Typography, ScrollButton, LandingImage, Footer } from 'shared/components/index';
+import eventLandingImage from 'assets/landing/event.jpg';
+import EventContent from './components/RantanganContent';
+
+export default function Rantangan() {
+	return (
+		<Box className='bg-[#FFFDF5]'>
+			<LandingImage bgImage={eventLandingImage} text='Our Events' />
+			<MainContainer>
+				<Box className='w-4/5'>
+					<Typography
+						variant='h1'
+						text='Connect with fellow Indonesians at our socials and large-scale events.'
+					/>
+				</Box>
+				<EventContent upcoming />
+				<EventContent upcoming={false} />
+				<ScrollButton threshold={7 / 10} />
+			</MainContainer>
+			<Footer />
+		</Box>
+	);
+}
