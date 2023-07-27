b32eb7b2d5da62d5a7f76e6ea51581abc3bc2d6b
Flayre <mariesamantha.f@gmail.com>
2023-07-27 11:04:29 -0700

    GW-72: Added rantangan event to home page

--- client/src/pages/home/components/events/constants.jsx
+++ client/src/pages/home/components/events/constants.jsx
@@ -1,5 +1,6 @@
 import rantangan from 'assets/home-page/events/rantangan.svg';
 import upcomingEvent from 'assets/home-page/events/upcoming_event.svg';
+import rantanganImage from 'assets/home-page/events/rantangan.png';
 
 /**
  * events field:
@@ -21,7 +22,14 @@ export const UPCOMINGEVENTS = {
 
 export const RANTANGAN = {
 	title: 'Rantangan',
-	events: [],
+	events: [
+		{
+			title: 'Rantangan 2023',
+			desc: ['Description of rantangan. Ideally 2-4 sentences only. A maximum of 3 lines for content.'],
+			img: <img src={rantanganImage} alt='UBUD: UBC Udah Dekat' />,
+			link: 'forms.gle/AXqcbDGtKBBiHCqX6',
+		},
+	],
 	icon: <img src={rantangan} alt='' className='ml-1/10' />,
 	button1: 'Pre-order',
 	button2: {
