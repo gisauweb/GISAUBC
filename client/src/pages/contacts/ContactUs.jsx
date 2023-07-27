764393dc4156d298f24d3d76c0d15b6914c0a413
Christopher Wong <chrishadrianwong@gmail.com>
2023-07-27 01:14:00 -0700

    GW-80: Updated contactus page

--- client/src/pages/contacts/ContactUs.jsx
+++ client/src/pages/contacts/ContactUs.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { Footer } from 'shared/components/index';
+import hands from 'assets/contact-us/hands.svg';
 import Title from './components/Title';
 import PlugBox from './components/PlugBox';
 import Location from './components/Location';
 
 export default function ContactUs() {
 	return (
-		<Box className='' sx={{ background: 'linear-gradient(#EDE6CB, #FFFDF5);' }}>
+		<Box className='relative' sx={{ background: 'linear-gradient(#EDE6CB, #FFFDF5);' }}>
 			<Title />
 			<Box className='w-4/5 mx-auto my-12 pb-12'>
 				<PlugBox />
 				<Location />
 			</Box>
+			<img src={hands} alt='hands-background-aesthetic' className='absolute top-1/4 right-0' />
 			<Footer />
 		</Box>
 	);
