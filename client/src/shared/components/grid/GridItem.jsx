66e8eab5a0616e89f79fed5f9b1f4702d8994bda
Flayre <mariesamantha.f@gmail.com>
2023-07-30 00:45:25 -0700

    GW-72: Moved item title z-index to upper layer

--- client/src/shared/components/grid/GridItem.jsx
+++ client/src/shared/components/grid/GridItem.jsx
@@ -15,7 +15,9 @@ export default function GridItem({ item, upcomingEvent, itemType }) {
 			<Box className={`flex ${isMobileView ? 'flex-col' : 'flex-row'} mx-7 sm:mx-0`}>
 				<img src={item.image} alt='item_image' className='rounded-2xl' style={{ zIndex: 5 }} />
 				<Box className={`flex flex-col ${isMobileView ? 'items-center' : 'ml-8 mt-5'} space-y-4`}>
-					<span className='font-semibold text-xl py-2'>{item.title}</span>
+					<span className='font-semibold text-xl py-2' style={{ zIndex: 5 }}>
+						{item.title}
+					</span>
 					<GridItemDescription item={item} itemType={itemType} upcomingEvent={upcomingEvent} />
 					<Button text='Pre-order' handleClickButton={() => handleClickButton(item.link)} />
 				</Box>
