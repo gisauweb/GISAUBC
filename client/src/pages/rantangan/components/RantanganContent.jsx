1e85fa0b2c729017f85b8477a80ac81e83f4eacc
Flayre <mariesamantha.f@gmail.com>
2023-07-31 02:11:49 -0700

    GW-72: Updated bakmi icon position, need to fix past rantangan text

--- client/src/pages/rantangan/components/RantanganContent.jsx
+++ client/src/pages/rantangan/components/RantanganContent.jsx
@@ -3,6 +3,7 @@ import { Box } from '@mui/material';
 import GridContainer from 'shared/components/grid/GridContainer';
 import { Typography, DropdownMenu } from 'shared/components/index';
 import UPCOMING_RANTANGAN from 'shared/data/upcoming_rantangan';
+import bakmi from 'assets/rantangan-page/bakmi.svg';
 import PAST_RANTANGAN from './constants';
 import GridContent from '../../../shared/components/grid/GridContent';
 
@@ -11,15 +12,22 @@ function RantanganContent({ upcoming }) {
 	const eventData = upcoming ? UPCOMING_RANTANGAN : PAST_RANTANGAN[selectedYear];
 
 	return (
-		<Box className='my-20 z-2'>
-			<Box className='flex flex-col w-full justify-between z-2'>
+		<Box className='my-20 relative'>
+			{!upcoming && (
+				<img
+					src={bakmi}
+					alt='bakmi-icon'
+					className='absolute w-[70%] sm:w-[30%] -right-[126px] z-[0] -top-20'
+				/>
+			)}
+			<Box className='flex flex-col w-full justify-between'>
 				<Typography
 					variant='h2'
 					text={`${upcoming ? 'UPCOMING' : 'PAST'} RANTANGAN`}
 					className={`pt-0.5 ${!upcoming && 'mb-3 sm:mb-0'}`}
 				/>
 				{!upcoming && (
-					<DropdownMenu selectedYear={selectedYear} setSelectedYear={setSelectedYear} source='Event' />
+					<DropdownMenu selectedYear={selectedYear} setSelectedYear={setSelectedYear} source='Rantangan' />
 				)}
 			</Box>
 			<GridContainer className='sm:my-28'>
