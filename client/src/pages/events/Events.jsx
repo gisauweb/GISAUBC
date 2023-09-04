40a9a24f327bdaa89b8b9f0a75281979d97907c3
Christopher Wong <chrishadrianwong@gmail.com>
2023-09-04 13:26:33 -0700

    GW-60: Updated Typography for Events page

--- client/src/pages/events/Events.jsx
+++ client/src/pages/events/Events.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import MainContainer from 'shared/layout/MainContainer';
-import { Typography, ScrollButton, LandingImage, Footer } from 'shared/components/index';
+import { ScrollButton, LandingImage, Footer } from 'shared/components/index';
 import eventLandingImage from 'assets/landing-image/event.jpg';
 import EventContent from './components/EventContent';
 
@@ -11,10 +11,9 @@ export default function Events() {
 			<LandingImage bgImage={eventLandingImage} text='Our Events' />
 			<MainContainer>
 				<Box className='w-4/5'>
-					<Typography
-						variant='h1'
-						text='Connect with fellow Indonesians at our socials and large-scale events.'
-					/>
+					<Typography variant='h3' color='primary'>
+						Connect with fellow Indonesians at our socials and large-scale events.
+					</Typography>
 				</Box>
 				<EventContent upcoming />
 				<EventContent upcoming={false} />
