7434381b2961787502e21a45c05d0d6ae8ffeeec
chw2305 <chw2305@students.cs.ubc.ca>
2023-09-18 16:59:14 -0700

    Updated website with soto

--- client/src/shared/data/upcoming_event.js
+++ client/src/shared/data/upcoming_event.js
@@ -1,4 +1,17 @@
-const UPCOMING_EVENTS = [];
+import { EVENTS_23_24 } from 'assets/events-page/past-events';
+import soto from 'assets/events-page/past-events/23_24/soto.pdf';
+
+const UPCOMING_EVENTS = [
+	{
+		image: EVENTS_23_24[2],
+		title: 'SOTO: Sobat Tongkrongan',
+		date: 'Friday, 29 September 2023 | 6PM-9PM',
+		loc: 'AMS Great Hall',
+		registrationLink:
+			'docs.google.com/forms/d/e/1FAIpQLSesRrCh1KmMN5lrxPMP0rjbXvL_kzCSjiDebBGjvLx9OjnRlQ/viewform?usp=sharing',
+		infoLink: soto,
+	},
+];
 
 export default UPCOMING_EVENTS;
 
