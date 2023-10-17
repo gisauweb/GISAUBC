6bba6658e9d3b9d8d9a122fa7080e55638667b22
chrishadrian <chrishadrianwong@gmail.com>
2023-10-17 15:17:17 -0700

    GW-90: Updated partner title

--- client/src/pages/partners/components/PartnerTitle.jsx
+++ client/src/pages/partners/components/PartnerTitle.jsx
@@ -5,7 +5,7 @@ export default function PartnerTitle() {
 	return (
 		<Box className='flex flex-col pb-3'>
 			<Typography variant='h2' color='primary' className='lg:pb-4'>
-				GISAU Partners
+				GISAU Year-Long Partners
 			</Typography>
 		</Box>
 	);
