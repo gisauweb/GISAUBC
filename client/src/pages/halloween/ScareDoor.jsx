48608c68b8ced0182a9ba5862e80b55116bdfba0
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2023-10-23 14:02:55 -0700

    GW-91: Make halloween content (#137)
    * GW-90: Updated partners list
    
    * GW-90: Updated partners components
    
    * GW-90: Updated responsiveness
    
    * GW-90: Updated indomie logo
    
    * GW-90: Fixed indomie image bug
    
    * GW-90: Updated description
    
    * GW-90: Updated partners list
    
    * GW-90: Shows non yearlong sponsors
    
    * GW-90: Updated partner title
    
    * GW-90: Updated sponsor registration
    
    * GW-90: Refactored partners
    
    * GW-91: Initial set up
    
    * GW-91: Changed halloween video
    
    * GW-91: Updated youtube video
    
    * GW-91: Updated ruhan door
    
    * GW-91: Updated door
    
    * GW-91: Updated youtube
    
    * GW-91: Test1
    
    * GW-91: Test2
    
    * GW-91: Test3
    
    * GW-91: Test4
    
    * GW-91: Test4
    
    * GW-91: Fixed audio bug
    
    * GW-91: Test5
    
    * GW-91: Reverted test5
    
    * GW-91: Updated audio
    
    * GW-91: Updated sound
    
    * GW-91: Added new event
    
    ---------
    
    Co-authored-by: gisauweb <116628210+gisauweb@users.noreply.github.com>

---
+++ client/src/pages/halloween/ScareDoor.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import './ScareDoor.css';
+import { useMediaQuery } from 'react-responsive';
+import ReactAudioPlayer from 'react-audio-player';
+import scaryAudio from 'assets/halloween/jump_scare.mp3';
+
+function ScareDoor() {
+	const [scareVisible, setScareVisible] = useState(false);
+	const [playScream, setPlayScream] = useState(false);
+	const [openDoor, setOpenDoor] = useState(false);
+	const [watchYoutube, setWatchYoutube] = useState(false);
+
+	const isMobileView = useMediaQuery({ query: '(max-width: 639px)' });
+
+	const handleScare = () => {
+		setOpenDoor(true);
+		setTimeout(() => {
+			setPlayScream(true);
+			setScareVisible(true);
+		}, 700);
+		setTimeout(() => {
+			setWatchYoutube(true);
+		}, 1500);
+	};
+
+	const playAudio = () => {
+		if (!playScream) {
+			setTimeout(() => {
+				setPlayScream(true);
+			}, 700);
+		}
+	};
+
+	const renderScareMaze = () => (
+		<div className={`maze-container ${scareVisible ? 'scare' : ''}`}>
+			<div className='flex flex-col items-center justify-center space-y-8'>
+				{!(playScream && scareVisible) && (
+					<>
+						<p>Don&apos;t open the door!</p>
+						<button type='button' onClick={playAudio}>
+							<button type='button' onClick={handleScare}>
+								{/* https://lenadesign.org/2021/04/26/css-door-animation-open-close-on-hover/ */}
+								<div className='door'>
+									<div className={`door-front ${openDoor ? 'door-open' : ''}`}>
+										<div className='knob' />
+									</div>
+									<div className='door-back'>
+										<div className='rack' />
+										<div className='hat' />
+										<div className='jacket' />
+									</div>
+								</div>
+							</button>
+						</button>
+					</>
+				)}
+				{playScream && <ReactAudioPlayer src={scaryAudio} autoPlay />}
+			</div>
+		</div>
+	);
+
+	const renderYoutube = () => (
+		<div className='flex flex-col justify-center items-center h-[100vh]'>
+			{isMobileView ? (
+				<iframe
+					width='300'
+					height='200'
+					src='https://www.youtube.com/embed/fKuUEBlkoJo?si=GDVxG_i-XTpA7wQr'
+					title='YouTube video player'
+					frameBorder='0'
+					allow='accelerometer; autoplay; clipboard-write;
+					encrypted-media; gyroscope; picture-in-picture; web-share'
+					allowFullScreen
+				/>
+			) : (
+				<iframe
+					width='560'
+					height='315'
+					src='https://www.youtube.com/embed/fKuUEBlkoJo?si=GDVxG_i-XTpA7wQr'
+					title='YouTube video player'
+					frameBorder='0'
+					allow='accelerometer; autoplay; clipboard-write;
+					encrypted-media; gyroscope; picture-in-picture; web-share'
+					allowFullScreen
+				/>
+			)}
+		</div>
+	);
+
+	return watchYoutube ? renderYoutube() : renderScareMaze();
+}
+
+export default ScareDoor;
