c841597b3c5a832201f265a2b63420af7eb38bf2
Sam <104884933+msamanthaf@users.noreply.github.com>
2023-11-09 18:13:28 -0800

    Update events with malem mingguan (#142)
    * Create sponsorship update branch
    
    * GW-92: Added partner contact section
    
    * GW-92: Changed capslock style to be uniform
    
    * GW-92: Removed halloween scaredoor page
    
    * GW-92: Changed style to button, reduce text size
    
    * GW-92: Changed button background color
    
    * GW-92: Moved rumah hantu to past events
    
    * GW-92: Added top margin for partner section divider
    
    * GW-92: Changed button action description
    
    * GW-92: Changed margin
    
    * GW-92: Customized margin acording to device
    
    * GW-92: Need larger margin
    
    * GW-92: Updated mobile view margin
    
    * Added new event
    
    * Updated assets
    
    ---------
    
    Co-authored-by: gisauweb <116628210+gisauweb@users.noreply.github.com>
    Co-authored-by: chrishadrian <chrishadrianwong@gmail.com>

--- client/src/shared/data/upcoming_event.js
+++ client/src/shared/data/upcoming_event.js
@@ -1,4 +1,16 @@
-const UPCOMING_EVENTS = [];
+import { EVENTS_23_24 } from 'assets/events-page/past-events';
+import malming from 'assets/events-page/malming.pdf';
+
+const UPCOMING_EVENTS = [
+	{
+		image: EVENTS_23_24[5],
+		title: 'Malem Mingguan',
+		date: 'Saturday, November 18 | 3:30PM',
+		loc: 'Consulate General of Indonesia (KJRI)',
+		registrationLink: 'https://clubs.getqpay.com/?eid=21979',
+		infoLink: malming,
+	},
+];
 
 export default UPCOMING_EVENTS;
 
