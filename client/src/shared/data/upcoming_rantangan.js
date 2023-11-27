a2a08fcecb1b5d25c1257a9a4dcfed370209b18a
chrishadrian <chrishadrianwong@gmail.com>
2023-11-27 14:54:03 -0800

    Updated study cafe

--- client/src/shared/data/upcoming_rantangan.js
+++ client/src/shared/data/upcoming_rantangan.js
@@ -1,4 +1,15 @@
-const UPCOMING_RANTANGAN = [];
+import rantangan from 'assets/rantangan-page/past-rantangan/23_24/1.svg';
+
+const UPCOMING_RANTANGAN = [
+	{
+		image: rantangan,
+		title: 'Beef Rice Bowls',
+		description: 'Served with Sambal Matah, Egg, and Cheese',
+		loc: 'AMS Lev Bukhaman',
+		price: '$13.5',
+		link: '',
+	},
+];
 
 export default UPCOMING_RANTANGAN;
 
@@ -6,7 +17,7 @@ export default UPCOMING_RANTANGAN;
 //	image: rantangan,
 //	title: 'title',
 //	description: 'desc',
-//  location:'loc',
+//  loc:'loc',
 //  price: '$10',
 //  link:'',
 // },
