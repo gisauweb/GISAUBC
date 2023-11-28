c5b5c0481439d69f2eebbe4833504ad18fafb267
chrishadrian <chrishadrianwong@gmail.com>
2023-11-27 16:53:18 -0800

    Removed upcoming event temp

--- client/src/shared/data/upcoming_rantangan.js
+++ client/src/shared/data/upcoming_rantangan.js
@@ -1,17 +1,17 @@
-// import rantangan from 'assets/rantangan-page/past-rantangan/23_24/1.svg';
+import rantangan from 'assets/rantangan-page/past-rantangan/23_24/1.svg';
 
 const UPCOMING_RANTANGAN = [
-	// {
-	// 	image: rantangan,
-	// 	title: 'Beef Rice Bowls',
-	// 	date: 'Wednesday, December 6 | 1-5:30PM',
-	// 	description: 'Served with Sambal Matah, Egg, and Cheese',
-	// 	loc: 'AMS Lev Bukhaman',
-	// 	price: '$13.5',
-	// 	registrationLink:
-	// 		'https://docs.google.com/forms/d/e/1FAIpQLSff_1vz9zTHLE_OJ0BzdGMFRFVW6Ni9K3249Fi8CtlH4tUUEg/viewform',
-	// 	infoLink: 'https://docs.google.com/forms/d/e/1FAIpQLSff_1vz9zTHLE_OJ0BzdGMFRFVW6Ni9K3249Fi8CtlH4tUUEg/viewform',
-	// },
+	{
+		image: rantangan,
+		title: 'Beef Rice Bowls',
+		date: 'Wednesday, December 6 | 1-5:30PM',
+		description: 'Served with Sambal Matah, Egg, and Cheese',
+		loc: 'AMS Lev Bukhaman',
+		price: '$13.5',
+		registrationLink:
+			'https://docs.google.com/forms/d/e/1FAIpQLSff_1vz9zTHLE_OJ0BzdGMFRFVW6Ni9K3249Fi8CtlH4tUUEg/viewform',
+		infoLink: 'https://docs.google.com/forms/d/e/1FAIpQLSff_1vz9zTHLE_OJ0BzdGMFRFVW6Ni9K3249Fi8CtlH4tUUEg/viewform',
+	},
 ];
 
 export default UPCOMING_RANTANGAN;
