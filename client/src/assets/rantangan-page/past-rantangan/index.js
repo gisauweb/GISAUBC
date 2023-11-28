a69a643d684ab228302d8a16a4cf6309e08c2967
chrishadrian <chrishadrianwong@gmail.com>
2023-11-27 17:38:55 -0800

    Removed image temp

--- client/src/assets/rantangan-page/past-rantangan/index.js
+++ client/src/assets/rantangan-page/past-rantangan/index.js
@@ -2,9 +2,9 @@
 import RANTANGAN_22_23_1 from './22_23/1.svg';
 import RANTANGAN_22_23_2 from './22_23/2.svg';
 
-import RANTANGAN_23_24_1 from './23_24/1.svg';
+// import RANTANGAN_23_24_1 from './22_23/1.svg';
 
-const RANTANGAN_23_24 = [RANTANGAN_23_24_1];
+const RANTANGAN_23_24 = [RANTANGAN_22_23_1];
 
 const RANTANGAN_22_23 = [RANTANGAN_22_23_1, RANTANGAN_22_23_2];
 
