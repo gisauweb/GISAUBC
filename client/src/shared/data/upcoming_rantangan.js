e1b9a31c7ad3e09035c672b85ebb28b6f8a05f64
chrishadrian <chrishadrianwong@gmail.com>
2023-11-27 17:20:08 -0800

    Added upcoming rantangan

--- client/src/shared/data/upcoming_rantangan.js
+++ client/src/shared/data/upcoming_rantangan.js
@@ -1,4 +1,16 @@
-const UPCOMING_RANTANGAN = [];
+import { RANTANGAN_23_24 } from 'assets/rantangan-page/past-rantangan/index';
+
+const UPCOMING_RANTANGAN = [
+	{
+		image: RANTANGAN_23_24[0],
+		title: 'Beef Rice Bowls',
+		date: 'Wednesday, December 6 | 1-5:30PM',
+		loc: 'AMS Lev Bukhman (Bird Nest)',
+		registrationLink:
+			'https://docs.google.com/forms/d/e/1FAIpQLSff_1vz9zTHLE_OJ0BzdGMFRFVW6Ni9K3249Fi8CtlH4tUUEg/viewform',
+		infoLink: 'https://docs.google.com/forms/d/e/1FAIpQLSff_1vz9zTHLE_OJ0BzdGMFRFVW6Ni9K3249Fi8CtlH4tUUEg/viewform',
+	},
+];
 
 export default UPCOMING_RANTANGAN;
 
