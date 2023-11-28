2213af93b2e970006193dc1ca6b2b4bf6548e534
chrishadrian <chrishadrianwong@gmail.com>
2023-11-27 18:06:30 -0800

    Fixed deploy errors

--- client/src/shared/data/upcoming_rantangan.js
+++ client/src/shared/data/upcoming_rantangan.js
@@ -1,19 +1,18 @@
-import rantangan from 'assets/rantangan-page/past-rantangan/23_24/1.svg';
+import { RANTANGAN_23_24 } from 'assets/rantangan-page/past-rantangan/index';
 
 const UPCOMING_RANTANGAN = [
 	{
-		image: rantangan,
+		image: RANTANGAN_23_24[0],
 		title: 'Beef Rice Bowls',
 		date: 'Wednesday, December 6 | 1-5:30PM',
-		description: 'Served with Sambal Matah, Egg, and Cheese',
-		loc: 'AMS Lev Bukhaman',
-		price: '$13.5',
+		loc: 'AMS Lev Bukhman (Bird Nest)',
 		registrationLink:
 			'https://docs.google.com/forms/d/e/1FAIpQLSff_1vz9zTHLE_OJ0BzdGMFRFVW6Ni9K3249Fi8CtlH4tUUEg/viewform',
 		infoLink: 'https://docs.google.com/forms/d/e/1FAIpQLSff_1vz9zTHLE_OJ0BzdGMFRFVW6Ni9K3249Fi8CtlH4tUUEg/viewform',
+		price: '$12 for members | $14 for non-members',
+		description: 'Served with Sambal Matah, Egg, and Cheese',
 	},
 ];
-
 export default UPCOMING_RANTANGAN;
 
 // {
