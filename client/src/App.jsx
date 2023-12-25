5933c3141fc90fc35fdb26674933bcdd96ca6749
chrishadrian <chrishadrianwong@gmail.com>
2023-12-24 17:36:45 -0800

    GW-94: Removed wildcard route temporarily

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Popup from 'pages/pop-up/Popup';
-import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { Box } from '@mui/material';
 import ScrollToTop from './shared/components/ScrollToTop';
 import NavigationBar from './shared/components/navigation-bar/NavigationBar';
@@ -15,7 +15,8 @@ function App() {
 	const location = useLocation();
 	// TODO: create a util function that all other pages can use to check if it's games page
 	const isGamesPage = location.pathname === '/games';
-	const shouldOpenPopup = !isGamesPage;
+	const activatePopup = false;
+	const shouldOpenPopup = activatePopup && !isGamesPage;
 
 	return (
 		<Box className='bg-[#FFFDF5]'>
@@ -26,7 +27,7 @@ function App() {
 						<Route key={page.name} path={page.path} element={page.element} />
 					))}
 				</Route>
-				<Route path='*' element={<Navigate replace to='/' />} />
+				{/* <Route path='*' element={<Navigate replace to='/' />} /> */}
 			</Routes>
 			{shouldOpenPopup && <Popup isOpen={isPopupOpen} onClose={handleClosePopup} />}
 		</Box>
