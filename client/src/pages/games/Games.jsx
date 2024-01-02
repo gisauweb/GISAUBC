81fa1a7f9e1b6c5fa8f3caec2f7cc4272c10af6e
chrishadrian <chrishadrianwong@gmail.com>
2024-01-02 15:54:02 -0800

    GW-94: Finalized end-to-end jwt authorization

--- client/src/pages/games/Games.jsx
+++ client/src/pages/games/Games.jsx
@@ -1,15 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useAuth0 } from '@auth0/auth0-react';
 import Dashboard from './dashboard/Dashboard';
 
 export default function Games() {
-	const { isAuthenticated, isLoading, loginWithPopup } = useAuth0();
+	const { isAuthenticated, isLoading, getAccessTokenSilently, loginWithPopup } = useAuth0();
+	const [token, setToken] = useState(null);
 
 	useEffect(() => {
+		async function getToken() {
+			const result = await getAccessTokenSilently();
+			// eslint-disable-next-line no-underscore-dangle
+			setToken(result);
+		}
 		if (!isAuthenticated) {
 			loginWithPopup();
+		} else {
+			getToken();
 		}
-	}, [isAuthenticated, loginWithPopup]);
-	return isAuthenticated && !isLoading ? <Dashboard /> : <div>Loading...</div>;
+	}, [isAuthenticated, loginWithPopup, getAccessTokenSilently]);
+
+	return isAuthenticated && !isLoading ? <Dashboard token={token} /> : <div>Loading...</div>;
 }
--- client/src/pages/games/dashboard/Dashboard.jsx
+++ client/src/pages/games/dashboard/Dashboard.jsx
@@ -1,27 +1,38 @@
-import Leaderboard from 'pages/games/dashboard/components/Leaderboard';
+import { useAuth0 } from '@auth0/auth0-react';
+// import Leaderboard from 'pages/games/dashboard/components/Leaderboard';
 import Sidebar from 'pages/games/dashboard/components/Sidebar';
-import queryString from 'query-string';
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 
-export default function Dashboard() {
-	const location = useLocation();
-	const { state } = queryString.parse(location.search);
+export default function Dashboard({ token }) {
 	const [challengesData, setChallengesData] = useState('none');
+	const { user } = useAuth0();
 
 	useEffect(() => {
-		if (state) {
-			fetch(`http://127.0.0.1:5001/gisaubc-dev/us-central1/api/challenges?code=${state}`)
+		if (token) {
+			fetch('http://127.0.0.1:5001/gisaubc-dev/us-central1/api/auth/challenges', {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+					Authorization: `Bearer ${token}`,
+				},
+			})
 				.then((res) => res.json())
 				.then((res) => setChallengesData(JSON.stringify(res)));
 		}
-	}, [state]);
+	}, [token]);
 
 	return (
-		<div className='flex flex-col'>
+		<div className='flex'>
 			<Sidebar />
-			<Leaderboard />
-			<div>{challengesData}</div>
+			{/* <Leaderboard /> */}
+			<div className='flex flex-col space-y-2'>
+				<p>{`Hi ${user.name}`}</p>
+				<p>
+					This is challenges data:
+					{challengesData}
+				</p>
+			</div>
 		</div>
 	);
 }
