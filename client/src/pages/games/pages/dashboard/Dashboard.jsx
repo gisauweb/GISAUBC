25488770c513acdcf0faf94e1b651b76336c1d0b
Flayre <mariesamantha.f@gmail.com>
2024-01-18 22:37:13 -0800

    GW-98: Added overlay bg

--- client/src/pages/games/pages/dashboard/Dashboard.jsx
+++ client/src/pages/games/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useMediaQuery } from 'react-responsive';
 import logo from 'assets/gisau-logo/gisau.svg';
 import sidemenu from 'assets/games/sidebar.svg';
+import { useSpring, animated } from 'react-spring';
 import Sidebar from '../Sidebar';
 import Profile from './components/Profile';
 import Points from './components/Points';
@@ -13,38 +14,45 @@ export default function Dashboard() {
 	const { user } = useAuth0();
 	const username = user.given_name || user.nickname;
 	const isMobileView = useMediaQuery({ query: '(max-width: 639px)' });
+	const [sidebarVisible, setSidebarVisible] = useState(false);
 
-	return user && !isMobileView ? (
-		<div className='flex h-screen'>
-			<Sidebar />
-			<div className='flex-1 flex flex-col items-center h-screen justify-center'>
-				<div className='flex h-1/3 w-4/5'>
-					<Profile username={username} picture={user.picture} />
-					<Points />
-				</div>
-				<div className='flex h-1/2 w-4/5'>
-					<Activity />
-				</div>
-			</div>
-			<Leaderboard username={username} />
-		</div>
-	) : (
+	const toggleSidebar = () => {
+		setSidebarVisible(!sidebarVisible);
+	};
+
+	const sidebarAnimation = useSpring({ left: sidebarVisible ? '0%' : '-80%' });
+
+	return (
 		user && (
-			<div className='flex flex-col h-fit mb-10 items-center gap-3 relative bg-bgPrimary'>
-				<div className='fixed z-50 left-0'>
+			<div
+				className={`flex flex-col h-fit mb-10 items-center gap-3 relative ${isMobileView ? 'bg-overlay' : ''}`}
+			>
+				{isMobileView && sidebarVisible && (
+					<div
+						className='fixed top-0 left-0 w-full h-full bg-black opacity-60 blur z-50'
+						onClick={toggleSidebar}
+					/>
+				)}
+				<animated.div
+					className='fixed z-50'
+					style={{
+						...sidebarAnimation,
+						position: 'fixed',
+						width: '80vw',
+					}}
+				>
 					<Sidebar username={username} picture={user.picture} className='fixed' />
-				</div>
+				</animated.div>
 				<div className='py-3'>
 					<div className='flex flex-row'>
-						<div className='flex flex-row fixed top-5 z-40 left-5'>
+						<div className='flex flex-row fixed top-5 z-40 left-5' onClick={toggleSidebar}>
 							<img
 								src={sidemenu}
-								href='/home'
 								alt='sidemenu'
 								style={{ maxWidth: '50px', height: 'auto', zIndex: '50', left: '0px' }}
 							/>
 						</div>
-						<div className='flex flex-row fixed top-5 z-50 right-5'>
+						<div className='flex flex-row fixed top-5 z-40 right-5'>
 							<img
 								src={logo}
 								href='/home'
