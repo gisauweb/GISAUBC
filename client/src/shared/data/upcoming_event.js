461ec1dbeb5501668315f4474c891614282a7860
Flayre <mariesamantha.f@gmail.com>
2024-01-28 18:11:32 -0800

    Add esports event

--- client/src/shared/data/upcoming_event.js
+++ client/src/shared/data/upcoming_event.js
@@ -1,23 +1,12 @@
 import { EVENTS_23_24 } from 'assets/events-page/past-events';
-import cafe from 'assets/events-page/past-events/23_24/cafe.pdf';
-import care from 'assets/events-page/past-events/23_24/care.pdf';
 
 const UPCOMING_EVENTS = [
 	{
-		image: EVENTS_23_24[7],
-		title: 'Exam Care Package',
-		date: 'Wednesday, December 5 | 12-5PM / Thursday, December 6 | 1-5:30PM ',
-		loc: 'AMS Nest',
-		registrationLink: 'https://forms.gle/uPPiTkc612KS7nZb9',
-		infoLink: care,
-	},
-	{
-		image: EVENTS_23_24[6],
-		title: 'Study Cafe',
-		date: 'Wednesday, December 6 | 1-5:30PM',
-		loc: 'AMS Lev Bukhman (Bird Nest)',
-		registrationLink: cafe,
-		infoLink: cafe,
+		image: EVENTS_23_24[8],
+		title: 'Bermain Bersama GISAU',
+		date: 'February 9-11, 12-4 PM',
+		loc: 'Metrotown',
+		registrationLink: 'https://forms.gle/pJjdw5Ac5B7MdnbPA',
 	},
 ];
 
