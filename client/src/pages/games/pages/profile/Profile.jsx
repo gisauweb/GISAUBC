3ae1aaefe3e53aebd99b82de462076536b966f47
chrishadrian <chrishadrianwong@gmail.com>
2024-01-30 01:33:05 -0800

    GW-115: implemented profile page

--- client/src/pages/games/pages/profile/Profile.jsx
+++ client/src/pages/games/pages/profile/Profile.jsx
@@ -1,13 +1,58 @@
-import { Box } from '@mui/material';
+import { Box, Button, TextField } from '@mui/material';
 import React from 'react';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 export default function Profile() {
 	return (
-		<Box>
-			<Box>{'< My profile'}</Box>
-			<Box>
-				<Box>Daily Challenges</Box>
-				<Box>My Badges</Box>
+		<Box className='flex px-7 py-16 justify-between w-full h-full'>
+			<Box className='w-1/2 flex flex-col justify-between px-5'>
+				<Box className='flex items-center space-x-2'>
+					<ArrowBackIosNewIcon sx={{ color: 'black', stroke: 'black', strokeWidth: 2 }} />
+					<span className='font-bold font-poppins text-2xl'>My Profile</span>
+				</Box>
+				<Box
+					className='self-center my-6'
+					sx={{ width: 140, height: 140, bgcolor: 'grey.300', borderRadius: '50%' }}
+				/>
+				<Box>
+					<TextField label='First name' variant='outlined' defaultValue='Jessie' fullWidth margin='normal' />
+					<TextField label='Last name' variant='outlined' defaultValue='Megan' fullWidth margin='normal' />
+					<TextField
+						label='Preferred name'
+						variant='outlined'
+						defaultValue='Jess'
+						fullWidth
+						margin='normal'
+					/>
+					<TextField
+						label='Student number'
+						variant='outlined'
+						defaultValue='12345678'
+						fullWidth
+						margin='normal'
+					/>
+					<TextField
+						label='Registered email address'
+						variant='outlined'
+						defaultValue='jessmgn@gisau.com'
+						fullWidth
+						margin='normal'
+					/>
+				</Box>
+				{/* Edit button */}
+				<Button variant='outlined' startIcon={<ArrowBackIosNewIcon />}>
+					Edit
+				</Button>
+			</Box>
+			<Box className='w-1/2 h-full flex flex-col justify-between'>
+				<Box className='h-1/2'>
+					<span className='font-bold font-poppins text-2xl'>Daily Challenges</span>
+					<span className='font-poppins text-xl'>coming soon...</span>
+				</Box>
+				<Box className='h-1/2'>
+					<span className='font-bold font-poppins text-2xl'>My Badges</span>
+					<span className='font-poppins text-xl'>coming soon...</span>
+				</Box>
 			</Box>
 		</Box>
 	);
