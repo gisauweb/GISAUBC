71c27488265c71d23ef7a3c244f100e071ecc280
chrishadrian <chrishadrianwong@gmail.com>
2024-01-31 18:10:45 -0800

    GW-115: Updated Profile's textfields

--- client/src/pages/games/pages/profile/Profile.jsx
+++ client/src/pages/games/pages/profile/Profile.jsx
@@ -1,40 +1,57 @@
-import { Box, Button } from '@mui/material';
+import { Box } from '@mui/material';
 import { Input } from '@material-tailwind/react';
-import React from 'react';
+import React, { useState } from 'react';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import EditButton from './components/EditButton';
 
-function StyledTextField({ value, label }) {
+function StyledTextField({ value, label, edit }) {
 	return (
 		<Box className='flex flex-col mb-5'>
 			<span className='text-lg font-poppins font-normal mb-1'>{label}</span>
-			<Input defaultValue={value} className='rounded-xl font-poppins text-base' disabled />
+			<Input
+				variant='outlined'
+				defaultValue={value}
+				className={`rounded-xl font-poppins text-base leading-none outline-1
+				outline-black ${edit ? 'text-black' : 'text-gray-400'}`}
+				containerProps={{ className: 'h-8' }}
+				disabled={!edit}
+			/>
 		</Box>
 	);
 }
 
-export default function Profile() {
+export default function Profile({ account }) {
+	const [edit, setEdit] = useState(false);
+
 	return (
-		<Box className='flex px-7 py-16 justify-between w-full h-[95%]'>
+		<Box className='flex px-7 py-16 justify-between w-[80vw] h-[95%]'>
 			<Box className='w-1/2 flex flex-col justify-between px-5'>
 				<Box className='flex items-center space-x-2'>
 					<ArrowBackIosNewIcon sx={{ color: 'black', stroke: 'black', strokeWidth: 2 }} />
 					<span className='font-bold font-poppins text-2xl'>My Profile</span>
 				</Box>
-				<Box
+				<img
+					src={account.profile_picture}
+					alt="User's profile"
+					style={{ width: 200, height: 200, borderRadius: '50%', borderWidth: '1px' }}
 					className='self-center my-6'
-					sx={{ width: 250, height: 250, bgcolor: 'grey.300', borderRadius: '50%' }}
 				/>
 				<Box className='px-20'>
-					<Box className='flex justify-between space-x-4'>
-						<StyledTextField value='Jessie' label='First name' />
-						<StyledTextField value='Megan' label='Last name' />
+					<Box className='flex justify-between '>
+						<StyledTextField value='Jessie' label='First name' edit={edit} />
+						<StyledTextField value='Megan' label='Last name' edit={edit} />
 					</Box>
-					<StyledTextField value='Jessie' fullWidth label='Preferred name' />
-					<StyledTextField value='12345678' fullWidth label='Student number' />
-					<StyledTextField value='jessmgn@gisau.com' fullWidth label='Registered email address' />
-					<Button variant='outlined' startIcon={<ArrowBackIosNewIcon />}>
-						Edit
-					</Button>
+					<StyledTextField value='Jessie' fullWidth label='Preferred name' edit={edit} />
+					<StyledTextField value='12345678' fullWidth label='Student number' edit={false} />
+					<StyledTextField
+						value='jessmgn@gisau.com'
+						fullWidth
+						label='Registered email address'
+						edit={false}
+					/>
+					<Box className='flex justify-end'>
+						<EditButton edit={edit} setEdit={setEdit} />
+					</Box>
 				</Box>
 			</Box>
 			<Box className='w-1/2 h-full flex flex-col justify-between'>
