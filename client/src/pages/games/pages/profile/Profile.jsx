794792ffb649a1fe9e3b51a946ead73e9775c95a
chrishadrian <chrishadrianwong@gmail.com>
2024-02-05 18:54:12 -0800

    GW-115: Initialized mobile view for profile page

--- client/src/pages/games/pages/profile/Profile.jsx
+++ client/src/pages/games/pages/profile/Profile.jsx
@@ -2,6 +2,7 @@ import { Box, IconButton } from '@mui/material';
 import { Input } from '@material-tailwind/react';
 import React, { useState } from 'react';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { useMediaQuery } from 'react-responsive';
 import EditButton from './components/EditButton';
 
 function StyledTextField({ value, label, edit, onChange }) {
@@ -24,34 +25,43 @@ function StyledTextField({ value, label, edit, onChange }) {
 export default function Profile({ account, token, setCurrentPage }) {
 	const [edit, setEdit] = useState(false);
 	const [nickname, setNickname] = useState(account.nickname);
+	const isMobileView = useMediaQuery({ query: '(max-width: 1039px)' });
 
 	const handleNicknameChange = (e) => {
 		setNickname(e.target.value);
 	};
 
 	return (
-		<Box className='flex px-7 py-16 justify-between w-[80vw] h-[95%]'>
-			<Box className='w-1/2 flex flex-col justify-between px-5'>
-				<Box className='flex items-center '>
-					<IconButton
-						onClick={() => {
-							setCurrentPage('Dashboard');
-						}}
-					>
-						<ArrowBackIosNewIcon
-							sx={{ color: 'black', stroke: 'black', strokeWidth: 3 }}
-							fontSize='small'
-						/>
-					</IconButton>
-					<span className='font-bold font-poppins text-2xl pl-0.5'>My Profile</span>
+		<Box
+			className='flex xl:px-7 xl:py-16 justify-center
+						xl:justify-between w-screen h-screen xl:w-[80vw] xl:h-[95%]'
+		>
+			<Box
+				className='w-full xl:w-1/2 flex flex-col justify-center items-center
+							xl:items-start xl:justify-between xl:px-5'
+			>
+				<Box className='flex items-center'>
+					{!isMobileView && (
+						<IconButton
+							onClick={() => {
+								setCurrentPage('Dashboard');
+							}}
+						>
+							<ArrowBackIosNewIcon
+								sx={{ color: 'black', stroke: 'black', strokeWidth: 3 }}
+								fontSize='small'
+							/>
+						</IconButton>
+					)}
+					<span className='font-bold font-poppins text-2xl xl:pl-0.5'>My Profile</span>
 				</Box>
 				<img
 					src={account.profile_picture}
 					alt="User's profile"
 					style={{ width: 200, height: 200, borderRadius: '50%', borderWidth: '1px' }}
-					className='self-center my-6'
+					className='self-center my-10 mb-16 xl:mb-6 xl:my-6'
 				/>
-				<Box className='px-20'>
+				<Box className='xl:px-20'>
 					<Box className='flex justify-between '>
 						<StyledTextField value={account.first_name} label='First name' edit={false} />
 						<StyledTextField value={account.last_name} label='Last name' edit={false} />
@@ -77,16 +87,18 @@ export default function Profile({ account, token, setCurrentPage }) {
 					</Box>
 				</Box>
 			</Box>
-			<Box className='w-1/2 h-full flex flex-col justify-between'>
-				<Box className='h-1/2 flex flex-col justify-between'>
-					<span className='font-bold font-poppins text-2xl'>Daily Challenges</span>
-					<span className='font-poppins text-xl relative h-1/2 mx-auto'>coming soon...</span>
+			{!isMobileView && (
+				<Box className='w-1/2 h-full flex flex-col justify-between'>
+					<Box className='h-1/2 flex flex-col justify-between'>
+						<span className='font-bold font-poppins text-2xl'>Daily Challenges</span>
+						<span className='font-poppins text-xl relative h-1/2 mx-auto'>coming soon...</span>
+					</Box>
+					<Box className='h-1/2 flex flex-col justify-between'>
+						<span className='font-bold font-poppins text-2xl'>My Badges</span>
+						<span className='font-poppins text-xl relative h-1/2 mx-auto'>coming soon...</span>
+					</Box>
 				</Box>
-				<Box className='h-1/2 flex flex-col justify-between'>
-					<span className='font-bold font-poppins text-2xl'>My Badges</span>
-					<span className='font-poppins text-xl relative h-1/2 mx-auto'>coming soon...</span>
-				</Box>
-			</Box>
+			)}
 		</Box>
 	);
 }
