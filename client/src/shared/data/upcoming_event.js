146447fe117cec734e7d312b6deee2c214385ed2
chrishadrian <chrishadrianwong@gmail.com>
2024-02-19 14:02:51 -0800

    Updated Indomie

--- client/src/shared/data/upcoming_event.js
+++ client/src/shared/data/upcoming_event.js
@@ -4,10 +4,10 @@ import indomie from 'assets/events-page/indomie.pdf';
 const UPCOMING_EVENTS = [
 	{
 		image: EVENTS_23_24[9],
-		title: 'Rumah Hantu',
-		date: 'Wednesday, October 27 | 3-8:30PM',
-		loc: 'AMS Nest Room 2314',
-		registrationLink: 'https://forms.gle/rDxe3Ho5aJn9aBiN9',
+		title: 'Indomie-T Me At The Finish Line',
+		date: 'Friday, March 1 | 4-7PM',
+		loc: 'Nest Lower Atrium',
+		registrationLink: 'https://forms.gle/YBcyyqDFsHu5kN9D8',
 		infoLink: indomie,
 	},
 ];
