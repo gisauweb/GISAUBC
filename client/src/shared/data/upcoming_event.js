43b778bc7696e1a8e4f7c372baec11bec5439813
chrishadrian <chrishadrianwong@gmail.com>
2024-02-19 13:56:43 -0800

    Add indomie

--- client/src/shared/data/upcoming_event.js
+++ client/src/shared/data/upcoming_event.js
@@ -1,6 +1,16 @@
-// import { EVENTS_23_24 } from 'assets/events-page/past-events';
+import { EVENTS_23_24 } from 'assets/events-page/past-events';
+import indomie from 'assets/events-page/indomie.pdf';
 
-const UPCOMING_EVENTS = [];
+const UPCOMING_EVENTS = [
+	{
+		image: EVENTS_23_24[9],
+		title: 'Rumah Hantu',
+		date: 'Wednesday, October 27 | 3-8:30PM',
+		loc: 'AMS Nest Room 2314',
+		registrationLink: 'https://forms.gle/rDxe3Ho5aJn9aBiN9',
+		infoLink: indomie,
+	},
+];
 
 export default UPCOMING_EVENTS;
 
