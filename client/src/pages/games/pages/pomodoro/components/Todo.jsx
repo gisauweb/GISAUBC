495c8223af91dc38368353dc89a013787725e91e
Flayre <mariesamantha.f@gmail.com>
2024-03-11 18:47:16 -0700

    Modified add task dialog box

--- client/src/pages/games/pages/pomodoro/components/Todo.jsx
+++ client/src/pages/games/pages/pomodoro/components/Todo.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Typography, Dialog, DialogTitle, DialogContent, TextField, Button, IconButton } from '@mui/material';
+import { Box, Typography, Dialog, DialogTitle, DialogContent, Button, IconButton } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -10,7 +10,7 @@ function Todo() {
 	const [open, setOpen] = useState(false);
 	const [newTask, setNewTask] = useState('');
 	const [description, setDescription] = useState('');
-	const [cycles, setCycles] = useState('');
+	const [cycles, setCycles] = useState(1);
 	const [selectedTaskIndex, setSelectedTaskIndex] = useState(null);
 	const [editedTaskIndex, setEditedTaskIndex] = useState(null);
 
@@ -95,7 +95,7 @@ function Todo() {
 				>
 					Task List
 				</Typography>
-				<Box className='flex flex-row gap-3'>
+				<Box className='flex flex-row gap-3 justify-center items-center'>
 					<AddIcon
 						onClick={handleOpen}
 						style={{ cursor: 'pointer', color: '#014900', width: '30px', height: '30px' }}
@@ -165,60 +165,73 @@ function Todo() {
 				</Box>
 			)}
 
-			<Dialog
-				open={open}
-				onClose={handleClose}
-				PaperProps={{ sx: { borderRadius: '20px', bgcolor: 'gamesBox' } }}
-			>
-				<DialogTitle>
-					<IconButton
-						aria-label='close'
-						onClick={handleClose}
-						sx={{ position: 'absolute', right: 0, top: 0 }}
-					>
-						<CloseIcon />
-					</IconButton>
-				</DialogTitle>
-				<DialogContent>
-					<Typography>Title</Typography>
-					<TextField
-						autoFocus
-						margin='dense'
-						type='text'
-						fullWidth
-						placeholder='Enter title'
-						value={newTask}
-						onChange={(e) => setNewTask(e.target.value)}
-					/>
-					<Typography>Description (optional)</Typography>
-					<TextField
-						margin='dense'
-						type='text'
-						fullWidth
-						placeholder='Enter description'
-						value={description}
-						onChange={(e) => setDescription(e.target.value)}
-					/>
-					<Box
-						sx={{
-							display: 'flex',
-							flexDirection: 'row',
-							alignItems: 'center',
-						}}
-					>
-						<Typography>Total cycles needed to complete task</Typography>
-						<TextField
-							type='number'
-							placeholder='Min. 1'
-							value={cycles}
-							onChange={(e) => setCycles(e.target.value)}
+			<Dialog open={open} onClose={handleClose} PaperProps={{ sx: { borderRadius: '20px' } }}>
+				<Box sx={{ backgroundColor: '#F5F1ED' }} className='flex flex-col'>
+					<DialogTitle>
+						<Typography sx={{ fontWeight: 'bold', textAlign: 'center', marginTop: '5px' }}>
+							Task Details
+						</Typography>
+						<IconButton
+							aria-label='close'
+							onClick={handleClose}
+							sx={{ position: 'absolute', right: 0, top: 0, color: '#BFA285' }}
+						>
+							<CloseIcon />
+						</IconButton>
+					</DialogTitle>
+					<DialogContent>
+						<Typography sx={{ fontWeight: 'bold' }}>Title</Typography>
+						<input
+							type='text'
+							placeholder='Enter task name'
+							value={newTask}
+							onChange={(e) => setNewTask(e.target.value)}
+							className='self-center flex-grow w-full h-12 px-4 mb-2 transition duration-200
+							 bg-white outline outline-none rounded-2xl shadow-sm appearance-none'
 						/>
+						<Typography sx={{ fontWeight: 'bold', marginTop: '5px' }}>Description (optional)</Typography>
+						<textarea
+							placeholder='Enter task description'
+							value={description}
+							onChange={(e) => setDescription(e.target.value)}
+							className='self-center flex-grow w-full h-24 px-4 mb-2 transition duration-200
+							 bg-white outline-none rounded-2xl shadow-sm appearance-none'
+							style={{ resize: 'none' }}
+						/>
+						<Box
+							sx={{
+								display: 'flex',
+								flexDirection: 'row',
+								gap: '10px',
+								marginTop: '5px',
+							}}
+							className='items-center'
+						>
+							<Typography sx={{ fontWeight: 'bold' }}>
+								Total cycles needed
+								<br />
+								to complete task
+							</Typography>
+							<input
+								type='number'
+								placeholder='1'
+								value={cycles}
+								onChange={(e) => setCycles(e.target.value)}
+								className='self-center w-1/5 px-4 transition duration-200
+								bg-white outline-none rounded-xl shadow-sm appearance-none ml-2'
+								style={{ borderRadius: '20px' }}
+							/>
+						</Box>
+					</DialogContent>
+					<Box sx={{ display: 'flex', justifyContent: 'end', p: 2 }}>
+						<Button
+							onClick={editedTaskIndex !== null ? editTask : addTask}
+							variant='contained'
+							sx={{ background: '#BFA285', color: 'white', borderRadius: '40px' }}
+						>
+							{editedTaskIndex !== null ? 'Update' : 'Done'}
+						</Button>
 					</Box>
-				</DialogContent>
-				<Box sx={{ display: 'flex', justifyContent: 'center', p: 2 }}>
-					<Button onClick={editedTaskIndex !== null ? editTask : addTask} variant='contained'>
-						{editedTaskIndex !== null ? 'Update' : 'Done'}
-					</Button>
 				</Box>
 			</Dialog>
 		</Box>
