2f30b538659ec04986b5dbda4e559c29aea05fcf
chrishadrian <chrishadrianwong@gmail.com>
2024-03-13 22:00:45 -0700

    GW-120: Made coming soon responsive

--- client/src/pages/games/pages/ComingSoon.jsx
+++ client/src/pages/games/pages/ComingSoon.jsx
@@ -3,7 +3,10 @@ import spaceship from 'assets/games/spaceship.gif';
 
 export default function ComingSoon() {
 	return (
-		<div className='flex items-center justify-center gap-x-5 pr-3 xl:w-[80%]'>
+		<div
+			className='flex flex-col xl:flex-row h-screen items-center justify-center
+						xl:gap-5 xl:w-[80%] pb-24 xl:pb-0 xl:pr-10'
+		>
 			<img src={spaceship} alt='Coming Soon' />
 			<div className='text-center'>
 				<p className='font-bold'>coming soon...</p>
