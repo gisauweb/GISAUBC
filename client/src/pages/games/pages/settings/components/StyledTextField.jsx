d8c1cc35f1518adbd26ce0abc4bf8a5c339e6a99
chrishadrian <chrishadrianwong@gmail.com>
2024-03-13 22:13:11 -0700

    GW-120: Refactored settings

---
+++ client/src/pages/games/pages/settings/components/StyledTextField.jsx
@@ -0,0 +1,19 @@
+import { Box } from '@mui/material';
+import { Input } from '@material-tailwind/react';
+
+export default function StyledTextField({ value, label, edit, onChange }) {
+	return (
+		<Box className='flex flex-col mb-5 px-3'>
+			<span className='text-lg font-poppins font-normal mb-1'>{label}</span>
+			<Input
+				variant='outlined'
+				value={value}
+				className={`rounded-xl font-poppins text-base leading-none outline-1
+				outline-black ${edit ? 'text-black' : 'text-gray-400'}`}
+				containerProps={{ className: 'h-8' }}
+				disabled={!edit}
+				onChange={onChange}
+			/>
+		</Box>
+	);
+}
