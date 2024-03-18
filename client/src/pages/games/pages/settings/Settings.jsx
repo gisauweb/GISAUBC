2d1a2b16d47e17c13e4edf99e6d24d7c5dfaa7de
chrishadrian <chrishadrianwong@gmail.com>
2024-03-18 00:43:10 -0700

    GW-120: Added settings to settings page

--- client/src/pages/games/pages/settings/Settings.jsx
+++ client/src/pages/games/pages/settings/Settings.jsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import Profile from './components/Profile';
+import ToggleSwitch from './components/ToggleSwitch';
 
 export default function Settings({ account, token, updateAccountState }) {
 	return (
@@ -11,14 +12,18 @@ export default function Settings({ account, token, updateAccountState }) {
 						xl:justify-between w-screen h-screen xl:w-full xl:h-full'
 			>
 				<Profile account={account} token={token} updateAccountState={updateAccountState} />
-				<Box className='w-1/2 h-full flex flex-col justify-between'>
-					<Box className='h-1/2 flex flex-col justify-between'>
+				<Box className='w-1/2 h-3/4 flex flex-col justify-between'>
+					<Box className='h-1/2 flex flex-col justify-start gap-y-[1.25rem]'>
 						<span className='font-bold font-poppins text-2xl'>Profile Visibility</span>
-						<span className='font-poppins text-xl relative h-1/2 mx-auto'>coming soon...</span>
+						<ToggleSwitch id='show_profile' label='Show my profile to others' />
+						<ToggleSwitch id='show_profile_picture' label='Show my profile picture to others' />
 					</Box>
-					<Box className='h-1/2 flex flex-col justify-between'>
+					<Box className='h-1/2 flex flex-col justify-start gap-y-[1.25rem]'>
 						<span className='font-bold font-poppins text-2xl'>Email Notifications</span>
-						<span className='font-poppins text-xl relative h-1/2 mx-auto'>coming soon...</span>
+						<ToggleSwitch id='show_profile' label='Daily reminders' />
+						<ToggleSwitch id='show_profile_picture' label='Weekly updates' />
+						<ToggleSwitch id='show_profile_picture' label='Weekly rewards info' />
+						<ToggleSwitch id='show_profile_picture' label='Weekly leaderboard' />
 					</Box>
 				</Box>
 			</Box>
