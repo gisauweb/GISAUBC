f0600ee8a0a118645d8e381449d6697f3a561545
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2024-03-18 15:51:11 -0700

    GW-126: Implemented mobile view for settings (#217)

--- client/src/pages/games/pages/settings/components/Profile.jsx
+++ client/src/pages/games/pages/settings/components/Profile.jsx
@@ -18,7 +18,7 @@ export default function Profile({ account, token, updateAccountState }) {
 			<Box className='flex xl:self-start'>
 				<span className='font-bold font-poppins text-2xl'>My profile</span>
 			</Box>
-			<Box className='flex flex-col h-full x-full'>
+			<Box className='flex flex-col h-full x-full px-[8vw] xl:px-0'>
 				<Box className='flex justify-center h-1/4 xl:h-1/3 my-10 xl:my-4'>
 					<img
 						src={account.profile_picture}
@@ -26,7 +26,7 @@ export default function Profile({ account, token, updateAccountState }) {
 						style={{ height: '100%', borderRadius: '50%', borderWidth: '1px' }}
 					/>
 				</Box>
-				<Box className='flex justify-between'>
+				<Box className='flex justify-between gap-x-5'>
 					<StyledTextField value={account.first_name} label='First name' edit={false} />
 					<StyledTextField value={account.last_name} label='Last name' edit={false} />
 				</Box>
@@ -39,7 +39,7 @@ export default function Profile({ account, token, updateAccountState }) {
 				/>
 				<StyledTextField value={account.sid} fullWidth label='Student number' edit={false} />
 				<StyledTextField value={account.email} fullWidth label='Registered email address' edit={false} />
-				<Box className='flex justify-end mt-7'>
+				<Box className='flex justify-end mt-2 xl:mt-0'>
 					<EditButton
 						edit={edit}
 						setEdit={setEdit}
--- client/src/pages/games/pages/settings/components/StyledTextField.jsx
+++ client/src/pages/games/pages/settings/components/StyledTextField.jsx
@@ -3,7 +3,7 @@ import { Input } from '@material-tailwind/react';
 
 export default function StyledTextField({ value, label, edit, onChange }) {
 	return (
-		<Box className='flex flex-col mb-5 px-3'>
+		<Box className='flex flex-col mb-5'>
 			<span className='text-lg font-poppins font-normal mb-1'>{label}</span>
 			<Input
 				variant='outlined'
