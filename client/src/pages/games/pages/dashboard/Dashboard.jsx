989ad8cb330d29b642365c76a1290ec430d1f767
chrishadrian <chrishadrianwong@gmail.com>
2024-03-18 03:03:18 -0700

    Disabled eslint

--- client/src/pages/games/pages/dashboard/Dashboard.jsx
+++ client/src/pages/games/pages/dashboard/Dashboard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useMediaQuery } from 'react-responsive';
