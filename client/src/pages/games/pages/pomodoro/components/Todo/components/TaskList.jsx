9b99bfd4f920592538458b19a176b675dd07f733
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2024-04-03 19:07:05 -0700

    GW-127: Integrate task routes to frontend (#229) (#230)
    * GW-127: Integrate task routes to frontend (#229)
    
    * GW-127: Updated task routing
    
    * GW-127: Updated task backend
    
    * GW-127: Refactored tasks, missing loading for async
    
    * GW-127: Fixed counter bug
    
    * GW-127: Fixed edit taskDialog
    
    * GW-127: Fix id-ing
    
    * GW-127: Fixed cycles counting on pomodoro
    
    * GW-127: Modified confirmation dialog
    
    * GW-127: Added archives feature
    
    * GW-127: Fixed checkmarks style

--- client/src/pages/games/pages/pomodoro/components/Todo/components/TaskList.jsx
+++ client/src/pages/games/pages/pomodoro/components/Todo/components/TaskList.jsx
@@ -1,67 +1,58 @@
 import React from 'react';
-import { Box, Typography, IconButton } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { useMediaQuery } from 'react-responsive';
 
-function TaskList({ tasks, handleTaskClick, handleTaskCompletion, selectedTaskIndex }) {
+function TaskList({ tasks, handleTaskClick, handleTaskCompletion, selectedTaskId, viewArchives }) {
 	const isMobileView = useMediaQuery({ query: '(max-width: 1039px)' });
-	return tasks.length === 0 ? (
-		<Box className={`flex flex-col justify-center self-center h-full ${isMobileView ? 'mt-60' : ''}`}>
-			<Typography className='text-slate-600'>No tasks left.</Typography>
-		</Box>
-	) : (
+	const filteredTasks = tasks.filter((task) => (viewArchives ? task.completed : !task.completed));
+
+	return (
 		<Box
-			className={`flex flex-col h-${isMobileView ? '96' : 'full'} 
-					${isMobileView ? 'mt-32' : 'mt-[33%]'} mb-1/10
-					w-full px-12 items-center`}
-			sx={{
-				overflowY: 'scroll',
-				scrollbarWidth: 'none',
-				'&::-webkit-scrollbar': { display: 'none' },
-			}}
+			className={`flex flex-col h-${isMobileView ? '96' : 'full'} ${
+				isMobileView ? 'mt-32' : 'mt-[33%]'
+			} mb-1/10 w-full px-12 items-center`}
+			sx={{ overflowY: 'scroll', scrollbarWidth: 'none', '&::-webkit-scrollbar': { display: 'none' } }}
 		>
-			{tasks
-				.slice(0)
-				.reverse()
-				.map((task, index) => (
+			{filteredTasks.length === 0 ? (
+				<Typography className='text-slate-600'>No tasks left.</Typography>
+			) : (
+				filteredTasks.map((task) => (
 					<Box
-						// eslint-disable-next-line react/no-array-index-key
-						key={index}
-						className='text-slate-600 bg-white rounded-2xl py-4 w-3/4
-							flex flex-row mb-4 gap-10 items-center'
+						key={task.title}
+						className='text-slate-600 bg-white rounded-2xl mb-4
+									py-4 w-3/4 flex flex-row gap-10 items-center'
 						style={{
-							borderLeft:
-								selectedTaskIndex === tasks.length - 1 - index
-									? '20px solid #732727'
-									: '20px solid white',
+							borderLeft: selectedTaskId === task.id ? '20px solid #732727' : '20px solid white',
 							cursor: 'pointer',
 						}}
-						onClick={() => handleTaskClick(tasks.length - 1 - index)}
+						onClick={() => handleTaskClick(task.id)}
 					>
 						<Box className='flex flex-col ml-5 w-full overflow-x-auto'>
 							<Typography style={{ fontWeight: 'bold' }}>{task.title}</Typography>
 							<Typography>{task.description}</Typography>
 						</Box>
-						<Box className='flex gap-3 w-1/5 mr-3 justify-end'>
-							<Typography style={{ fontWeight: 'bold' }}>{task.cycles}</Typography>
-							<IconButton
-								onClick={() => handleTaskCompletion(tasks.length - 1 - index)}
-								style={{
-									backgroundColor: task.complete ? 'green' : 'grey',
-									width: '25px',
-									height: '25px',
-									display: 'flex',
-									justifyContent: 'center',
-									alignItems: 'center',
+						<Box className='flex gap-3 w-2/5 pr-3 justify-end'>
+							<Typography style={{ fontWeight: 'bold' }}>{`${task.cycles}/${task.target}`}</Typography>
+							<Box
+								onClick={(e) => {
+									e.stopPropagation();
+									handleTaskCompletion(task.id);
 								}}
-								className='items-center'
+								style={{
+									cursor: 'pointer',
+									backgroundColor: task.completed ? 'green' : 'grey',
+									borderRadius: '50%',
+								}}
+								className='flex justify-center items-center w-[30px] h-[30px]'
 							>
-								<span style={{ color: 'white', fontSize: '16px' }}>✔</span>
-							</IconButton>
+								<Typography style={{ color: 'white' }}>✔</Typography>
+							</Box>
 						</Box>
 					</Box>
 				))
-				.reverse()}
+			)}
 		</Box>
 	);
 }
+
 export default TaskList;
