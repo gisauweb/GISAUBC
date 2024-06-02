345d4ad68758122b5108fea278fc5fff702e2fa4
chrishadrian <chrishadrianwong@gmail.com>
2024-06-02 16:11:52 -0700

    Card problem test

--- client/src/pages/games/pages/Sidebar/Sidebar.jsx
+++ client/src/pages/games/pages/Sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-wrap-multilines */
 /* eslint-disable react/jsx-indent */
 /* eslint-disable object-curly-newline */
@@ -94,147 +95,148 @@ export default function Sidebar({ username, picture, onCloseSidebar, currentPage
 	const isCurrentPage = (page) => currentPage === page;
 
 	return (
-		<Card
-			className='h-full max-w-[18rem] p-4
-			shadow-xl shadow-blue-gray-900/5 bg-gamesRed'
-			style={{
-				height: '100dvh',
-				width: isMobileView ? '75vw' : '20vw',
-				borderRadius: '0 30px 30px 0',
-				color: 'white',
-			}}
-		>
-			<div
-				className='mb-2 gap-4 p-4'
-				style={{
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'flex-start',
-					marginTop: '10px',
-				}}
-			>
-				{isMobileView ? (
-					<div onClick={handleCloseSidebar}>
-						<img
-							src={close}
-							alt='close'
-							style={{ maxWidth: '70px', height: 'auto', marginBottom: '20px' }}
-						/>
-					</div>
-				) : (
-					<img src={logo} href='/home' alt='GISAU logo red' style={{ maxWidth: '70px', height: 'auto' }} />
-				)}
+		// <Card
+		// 	className='h-full max-w-[18rem] p-4
+		// 	shadow-xl shadow-blue-gray-900/5 bg-gamesRed'
+		// 	style={{
+		// 		height: '100dvh',
+		// 		width: isMobileView ? '75vw' : '20vw',
+		// 		borderRadius: '0 30px 30px 0',
+		// 		color: 'white',
+		// 	}}
+		// >
+		// 	<div
+		// 		className='mb-2 gap-4 p-4'
+		// 		style={{
+		// 			display: 'flex',
+		// 			flexDirection: 'column',
+		// 			alignItems: 'flex-start',
+		// 			marginTop: '10px',
+		// 		}}
+		// 	>
+		// 		{isMobileView ? (
+		// 			<div onClick={handleCloseSidebar}>
+		// 				<img
+		// 					src={close}
+		// 					alt='close'
+		// 					style={{ maxWidth: '70px', height: 'auto', marginBottom: '20px' }}
+		// 				/>
+		// 			</div>
+		// 		) : (
+		// 			<img src={logo} href='/home' alt='GISAU logo red' style={{ maxWidth: '70px', height: 'auto' }} />
+		// 		)}
 
-				<Typography variant='h5' color='blue-gray'>
-					GISAU GAMES
-				</Typography>
-				<img src={rectangle} alt='border' style={{ maxWidth: '100px', height: 'auto', marginTop: '5px' }} />
-			</div>
-			<List>
-				<SelectableListItem
-					prefixIcon={IoMdHome}
-					label='Dashboard'
-					isSelected={isCurrentPage('Dashboard')}
-					onClick={() => handleMenuItemClick('Dashboard')}
-				/>
-				<Accordion
-					open={open === 1}
-					icon={
-						<ChevronDownIcon
-							strokeWidth={2.5}
-							className={`mx-auto h-4 w-4 transition-transform 
-			${open === 1 ? 'rotate-180' : ''}`}
-						/>
-					}
-				>
-					<ListItem className='p-0' selected={open === 1}>
-						<AccordionHeader onClick={() => handleOpen(1)} className='border-b-0 p-3'>
-							<ListItemPrefix>
-								<FaGamepad className='h-5 w-5' />
-							</ListItemPrefix>
-							<Typography color='blue-gray' className='mr-auto font-normal'>
-								Games
-							</Typography>
-						</AccordionHeader>
-					</ListItem>
-					<AccordionBody className='py-1' style={{ color: 'white' }}>
-						<List className='p-0'>
-							<SelectableListItem
-								prefixIcon={ChevronRightIcon}
-								label='Pomodoro Timer'
-								isSelected={isCurrentPage('Pomodoro')}
-								onClick={() => handleMenuItemClick('Pomodoro')}
-							/>
-						</List>
-					</AccordionBody>
-				</Accordion>
-				<SelectableListItem
-					prefixIcon={FaGift}
-					label='Redeem Points'
-					isSelected={isCurrentPage('Redeem')}
-					onClick={() => handleMenuItemClick('Redeem')}
-				/>
-				<SelectableListItem
-					prefixIcon={IoMdSettings}
-					label='Settings'
-					isSelected={isCurrentPage('Settings')}
-					onClick={() => handleMenuItemClick('Settings')}
-				/>
-			</List>
-			<List style={{ position: 'absolute', bottom: '10px' }}>
-				{isMobileView && (
-					<>
-						<ListItem onClick={() => handleMenuItemClick('Profile')} className='left-[-1] ml-[-1]'>
-							<div
-								className='justify-center'
-								style={{
-									width: '50px',
-									height: '50px',
-									marginRight: '5px',
-								}}
-							>
-								<img
-									src={picture}
-									alt='profile_pic'
-									style={{
-										width: 'auto',
-										height: 'auto',
-										objectFit: 'cover',
-										borderRadius: '50%',
-									}}
-								/>
-							</div>
-							<div
-								className='justify-center'
-								style={{
-									width: '80%',
-									overflow: 'hidden',
-									maxWidth: '80%',
-								}}
-							>
-								<Typography style={{ fontWeight: 'bold', marginLeft: '5px' }}>{username}</Typography>
-							</div>
-						</ListItem>
-						<ListItem onClick={() => handleMenuItemClick('Profile')}>
-							<ListItemPrefix>
-								<PersonIcon className='h-5 w-5' />
-							</ListItemPrefix>
-							View Profile
-						</ListItem>
-					</>
-				)}
-				<ListItem onClick={() => handleLogout()}>
-					<ListItemPrefix>
-						<FaSignOutAlt className='h-5 w-5 ml-1' />
-					</ListItemPrefix>
-					Sign Out
-				</ListItem>
-			</List>
-			<ConfirmationDialog
-				open={showLeaveConfirmation}
-				onClose={handleCloseLeaveDialog}
-				onLeave={handleLeaveConfirm}
-			/>
-		</Card>
+		// 		<Typography variant='h5' color='blue-gray'>
+		// 			GISAU GAMES
+		// 		</Typography>
+		// 		<img src={rectangle} alt='border' style={{ maxWidth: '100px', height: 'auto', marginTop: '5px' }} />
+		// 	</div>
+		// 	<List>
+		// 		<SelectableListItem
+		// 			prefixIcon={IoMdHome}
+		// 			label='Dashboard'
+		// 			isSelected={isCurrentPage('Dashboard')}
+		// 			onClick={() => handleMenuItemClick('Dashboard')}
+		// 		/>
+		// 		<Accordion
+		// 			open={open === 1}
+		// 			icon={
+		// 				<ChevronDownIcon
+		// 					strokeWidth={2.5}
+		// 					className={`mx-auto h-4 w-4 transition-transform
+		// 	${open === 1 ? 'rotate-180' : ''}`}
+		// 				/>
+		// 			}
+		// 		>
+		// 			<ListItem className='p-0' selected={open === 1}>
+		// 				<AccordionHeader onClick={() => handleOpen(1)} className='border-b-0 p-3'>
+		// 					<ListItemPrefix>
+		// 						<FaGamepad className='h-5 w-5' />
+		// 					</ListItemPrefix>
+		// 					<Typography color='blue-gray' className='mr-auto font-normal'>
+		// 						Games
+		// 					</Typography>
+		// 				</AccordionHeader>
+		// 			</ListItem>
+		// 			<AccordionBody className='py-1' style={{ color: 'white' }}>
+		// 				<List className='p-0'>
+		// 					<SelectableListItem
+		// 						prefixIcon={ChevronRightIcon}
+		// 						label='Pomodoro Timer'
+		// 						isSelected={isCurrentPage('Pomodoro')}
+		// 						onClick={() => handleMenuItemClick('Pomodoro')}
+		// 					/>
+		// 				</List>
+		// 			</AccordionBody>
+		// 		</Accordion>
+		// 		<SelectableListItem
+		// 			prefixIcon={FaGift}
+		// 			label='Redeem Points'
+		// 			isSelected={isCurrentPage('Redeem')}
+		// 			onClick={() => handleMenuItemClick('Redeem')}
+		// 		/>
+		// 		<SelectableListItem
+		// 			prefixIcon={IoMdSettings}
+		// 			label='Settings'
+		// 			isSelected={isCurrentPage('Settings')}
+		// 			onClick={() => handleMenuItemClick('Settings')}
+		// 		/>
+		// 	</List>
+		// 	<List style={{ position: 'absolute', bottom: '10px' }}>
+		// 		{isMobileView && (
+		// 			<>
+		// 				<ListItem onClick={() => handleMenuItemClick('Profile')} className='left-[-1] ml-[-1]'>
+		// 					<div
+		// 						className='justify-center'
+		// 						style={{
+		// 							width: '50px',
+		// 							height: '50px',
+		// 							marginRight: '5px',
+		// 						}}
+		// 					>
+		// 						<img
+		// 							src={picture}
+		// 							alt='profile_pic'
+		// 							style={{
+		// 								width: 'auto',
+		// 								height: 'auto',
+		// 								objectFit: 'cover',
+		// 								borderRadius: '50%',
+		// 							}}
+		// 						/>
+		// 					</div>
+		// 					<div
+		// 						className='justify-center'
+		// 						style={{
+		// 							width: '80%',
+		// 							overflow: 'hidden',
+		// 							maxWidth: '80%',
+		// 						}}
+		// 					>
+		// 						<Typography style={{ fontWeight: 'bold', marginLeft: '5px' }}>{username}</Typography>
+		// 					</div>
+		// 				</ListItem>
+		// 				<ListItem onClick={() => handleMenuItemClick('Profile')}>
+		// 					<ListItemPrefix>
+		// 						<PersonIcon className='h-5 w-5' />
+		// 					</ListItemPrefix>
+		// 					View Profile
+		// 				</ListItem>
+		// 			</>
+		// 		)}
+		// 		<ListItem onClick={() => handleLogout()}>
+		// 			<ListItemPrefix>
+		// 				<FaSignOutAlt className='h-5 w-5 ml-1' />
+		// 			</ListItemPrefix>
+		// 			Sign Out
+		// 		</ListItem>
+		// 	</List>
+		// 	<ConfirmationDialog
+		// 		open={showLeaveConfirmation}
+		// 		onClose={handleCloseLeaveDialog}
+		// 		onLeave={handleLeaveConfirm}
+		// 	/>
+		// </Card>
+		<div>abc</div>
 	);
 }
