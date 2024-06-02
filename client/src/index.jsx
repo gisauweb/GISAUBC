f4025a186e8dc3fe3dc4754cd8e7ce24e2114dd0
chrishadrian <chrishadrianwong@gmail.com>
2024-06-02 16:48:49 -0700

    Updated package json

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 import theme from 'libs/theme';
 import { ThemeProvider, responsiveFontSizes } from '@mui/material';
-import { ThemeProvider as TailwindThemeProvider } from '@material-tailwind/react';
 import { Auth0Provider } from '@auth0/auth0-react';
 import getAuthConfig from 'libs/config';
 import history from 'libs/history';
@@ -38,9 +37,7 @@ root.render(
 		<BrowserRouter>
 			<Auth0Provider {...providerConfig}>
 				<ThemeProvider theme={responsiveTheme}>
-					<TailwindThemeProvider>
-						<App />
-					</TailwindThemeProvider>
+					<App />
 				</ThemeProvider>
 			</Auth0Provider>
 		</BrowserRouter>
