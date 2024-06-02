dd9cf6419f54fb0d6b188735f375e74e3df436e4
chrishadrian <chrishadrianwong@gmail.com>
2024-06-02 15:55:47 -0700

    Added more logs

--- client/src/pages/games/pages/Sidebar/Sidebar.jsx
+++ client/src/pages/games/pages/Sidebar/Sidebar.jsx
@@ -11,7 +11,6 @@ import {
 	Accordion,
 	AccordionHeader,
 	AccordionBody,
-	useTheme,
 } from '@material-tailwind/react';
 import logo from 'assets/gisau-logo/gisau.svg';
 import rectangle from 'assets/games/Rectangle.png';
@@ -44,9 +43,6 @@ function SelectableListItem({ prefixIcon: PrefixIcon, label, isSelected, onClick
 }
 
 export default function Sidebar({ username, picture, onCloseSidebar, currentPage, setCurrentPage }) {
-	const theme = useTheme();
-	// eslint-disable-next-line no-console
-	console.log('theme: ', theme);
 	const [open, setOpen] = useState(0);
 	const [showLeaveConfirmation, setShowLeaveConfirmation] = useState(false);
 	const [nextPage, setNextPage] = useState(null);
