e77d2c3eab015ab32ea1f4b67920e880993b6a84
angelafeliciaa <angela_felicia@yahoo.com>
2024-07-06 11:15:27 -0700

    GW-integrate-isGamesPage-util

--- client/src/shared/components/navigation-bar/components/DesktopNavBar.jsx
+++ client/src/shared/components/navigation-bar/components/DesktopNavBar.jsx
@@ -2,6 +2,7 @@ import { Box } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import GameMenu from './GameMenu';
+import isGamesPage from '../../../../routeUtils';
 
 export default function DesktopNavBar({ hasLandingImage, bgColor, pages, location }) {
 	const ScrollToTop = () => {
@@ -15,25 +16,21 @@ export default function DesktopNavBar({ hasLandingImage, bgColor, pages, locatio
 					hasLandingImage ? bgColor : 'bg-white bg-opacity-70'
 				} h-14 rounded-[15px]`}
 			>
-				{pages.map((page) => {
-					const isGamesPage = page.name === 'Games';
-
-					return (
-						<Link key={page.name} to={page.path} className='px-5 pt-3' onClick={ScrollToTop} target='_self'>
-							{isGamesPage ? (
-								<GameMenu hasLandingImage={hasLandingImage} page={page} location={location} />
-							) : (
-								<p
-									className={`underline-animation font-oswald text-xl
-									${hasLandingImage ? 'text-white' : 'text-primary underline-animation-red'}
-									${page.path === location.pathname && 'underlined'}`}
-								>
-									{page.name}
-								</p>
-							)}
-						</Link>
-					);
-				})}
+				{pages.map((page) => (
+					<Link key={page.name} to={page.path} className='px-5 pt-3' onClick={ScrollToTop} target='_self'>
+						{isGamesPage(location.pathname) ? (
+							<GameMenu hasLandingImage={hasLandingImage} page={page} location={location} />
+						) : (
+							<p
+								className={`underline-animation font-oswald text-xl
+								${hasLandingImage ? 'text-white' : 'text-primary underline-animation-red'}
+								${page.path === location.pathname && 'underlined'}`}
+							>
+								{page.name}
+							</p>
+						)}
+					</Link>
+				))}
 			</Box>
 		</Box>
 	);
