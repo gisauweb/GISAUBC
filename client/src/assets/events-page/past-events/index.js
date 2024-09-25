88a134c1befab887f9e7da570c7e5e733ebb02a4
msamanthaf <mariesamantha.f@gmail.com>
2024-09-24 21:16:28 -0700

    GW-145: Revised SOTO

--- client/src/assets/events-page/past-events/index.js
+++ client/src/assets/events-page/past-events/index.js
@@ -31,7 +31,7 @@ import EVENT_23_24_14 from './23_24/14.svg';
 
 import EVENT_24_25_1 from './24_25/1.svg';
 import EVENT_24_25_2 from './24_25/2.svg';
-import EVENT_24_25_3 from './24_25/3.webp';
+import EVENT_24_25_3 from './24_25/3.svg';
 
 const EVENTS_24_25 = [EVENT_24_25_1, EVENT_24_25_2, EVENT_24_25_3];
 
