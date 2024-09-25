f23444d3fada8fbbd18726b2534b42f1f5362c90
msamanthaf <mariesamantha.f@gmail.com>
2024-09-24 21:38:02 -0700

    GW-145: Fixed sponsors list

--- client/src/shared/data/partners.js
+++ client/src/shared/data/partners.js
@@ -1,17 +1,35 @@
 /* eslint-disable max-len */
-import wizeprep from 'assets/partners/wizeprep.svg';
-import steves from 'assets/partners/steves.svg';
 import rainbowSVG from 'assets/home-page/rainbow.svg';
-import bananaleaf from 'assets/partners/bananaLeaf.svg';
+import bananaLeaf from 'assets/partners/bananaLeaf.svg';
+import honeydewLabs from 'assets/partners/honeydewLabs.svg';
+import indomie from 'assets/partners/indomie.svg';
 import prestigeOptical from 'assets/partners/prestigeOptical.svg';
+import stevesPoke from 'assets/partners/steves.svg';
 import storageHotel from 'assets/partners/storageHotel.svg';
+import wizePrep from 'assets/partners/wizeprep.svg';
 
 const PARTNERS = [
 	{
 		name: 'Banana Leaf Vancouver',
-		logo: bananaleaf,
+		logo: bananaLeaf,
 		description:
 			'Banana Leaf is a journey through Malaysian and Southeast Asian flavors and hospitality. Founded in 1995, we’ve grown from a cozy spot to five locations across Metro Vancouver. Blending Malaysian roots with influences from Indonesia, Singapore, Thailand, China, and India, we offer unique dishes bursting with diverse flavors.',
+		link: 'https://www.bananaleaf-vancouver.com/',
+		yearlong: true,
+	},
+	{
+		name: 'Honeydew Labs',
+		logo: honeydewLabs,
+		description:
+			'Honeydew Labs is dedicated to delivering natural, proven skincare ingredients that are effortlessly enjoyable for daily use. We firmly believe that skincare should be a delightful journey, fostering self-love and empowerment, while leaving unrealistic standards of perfection behind.',
+		link: 'https://www.honeydewlabs.com/',
+		yearlong: true,
+	},
+	{
+		name: 'Indomie',
+		logo: indomie,
+		description:
+			'Indomie is produced by Indofood, the pioneer of instant noodles in Indonesia and is one of the largest instant noodles manufacturers in the world. Indomie comes in many varieties from our classic soup flavours to our most popular dry noodle flavour Indomie Mi Goreng!',
 		link: 'https://indomieonline.ca/',
 		yearlong: true,
 	},
@@ -20,12 +38,12 @@ const PARTNERS = [
 		logo: prestigeOptical,
 		description:
 			'Prestige Optical is a one stop boutique optical store located in Kitsilano, Vancouver, founded and operated by Licensed Opticians with over 70 years of lineage in vision care providing high-quality eye care and fashionable eyewear.',
-		link: 'https://www.lemonjarz.com/',
+		link: 'https://prestigeoptical.ca/',
 		yearlong: true,
 	},
 	{
 		name: "Steve's Poke bar",
-		logo: steves,
+		logo: stevesPoke,
 		description:
 			"Discover authentic Hawaiian poké at Steve's Poké Bar in Metro Vancouver. Our house-made sauce marinated poké sets us apart from other spots",
 		link: 'https://www.stevespokebar.ca/',
@@ -36,12 +54,12 @@ const PARTNERS = [
 		logo: storageHotel,
 		description:
 			"Storagehotel is UBC and UofT's #1 student storage service! We pick up, store, and deliver. Free boxes and tape. Starting at $5/month. Upfront pricing.",
-		link: 'https://milkcowcafe.ca/',
+		link: 'https://www.storagehotel.ca/',
 		yearlong: true,
 	},
 	{
 		name: 'Wizeprep',
-		logo: wizeprep,
+		logo: wizePrep,
 		description:
 			'Wizeprep is an all-in-one education platform offering instant online access to course-tailored university exam-prep, tutorials, expert tutors, and many more study tools. This cutting-edge platform delivers a seamless learning experience stocked with all the tools needed to help students reach their full academic potential.',
 		link: 'https://www.wizeprep.com/',
