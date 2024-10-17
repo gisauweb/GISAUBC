72a4a217d3ec857ed7c374f63155667a3e9dd9c0
Jennifer Tjen <jennifer.s.e.tjen@gmail.com>
2024-10-17 15:50:58 -0700

    GW-143: Special for GISAU Partners Section

--- client/src/pages/partners/components/PartnerTitle.jsx
+++ client/src/pages/partners/components/PartnerTitle.jsx
@@ -1,12 +1,16 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
+import shineIcon from 'assets/partners/shineIcon.svg';
 
 export default function PartnerTitle() {
 	return (
-		<Box className='flex flex-col pb-3'>
-			<Typography variant='h2' color='primary' className='lg:pb-4'>
-				GISAU Year-Long Partners
-			</Typography>
+		<Box className='flex w-full pb-3 align-bottom'>
+			<img src={shineIcon} alt='stars.png' />
+			<div className='flex flex-col justify-end'>
+				<Typography variant='h4' color='primary' className=''>
+					Our Year-Long Partners
+				</Typography>
+			</div>
 		</Box>
 	);
 }
--- client/src/pages/partners/components/index.js
+++ client/src/pages/partners/components/index.js
@@ -3,5 +3,6 @@ import PartnerList from './PartnerList';
 import PartnerTitle from './PartnerTitle';
 import PartnerRegistration from './PartnerRegistration';
 import PartnerContact from './PartnerContact';
+import PartnerSpecial from './PartnerSpecial';
 
-export { PartnerContainer, PartnerList, PartnerRegistration, PartnerTitle, PartnerContact };
+export { PartnerContainer, PartnerList, PartnerRegistration, PartnerTitle, PartnerContact, PartnerSpecial };
