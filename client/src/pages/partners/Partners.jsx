b9cf312f3e66d53f8e742ebf6741edfbea91c6cd
Matthew <mharyant@students.cs.ubc.ca>
2024-11-01 18:09:41 -0700

    GW-142: implement button

--- client/src/pages/partners/Partners.jsx
+++ client/src/pages/partners/Partners.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { ScrollButton, LandingImage, Footer } from 'shared/components/index';
 import partnersLandingImg from 'assets/landing-image/partners.webp';
-import { Footer, LandingImage, ScrollButton } from 'shared/components/index';
 import MainContainer from 'shared/layout/MainContainer';
 import { PartnerTitle, PartnerList, PartnerContact, PartnerSpecial, PartnerHeadline } from './components/index';
 import PartnerButton from './components/PartnerButton';
