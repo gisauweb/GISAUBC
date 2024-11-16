98ae21a78a0941e28c3b65b4a3652cf9e002205a
gisauweb <gisauweb@gmail.com>
2024-11-16 02:56:10 -0800

    GW-166: Make logo scroll to top, added colors

--- client/src/shared/components/navigation-bar/components/DesktopNavBar.jsx
+++ client/src/shared/components/navigation-bar/components/DesktopNavBar.jsx
@@ -34,7 +34,7 @@ export default function DesktopNavBar({ bgColor, hasLandingImage, pages, locatio
 			<Box className='w-1/3 flex justify-end'>
 				<Link
 					to='https://forms.gle/33ovq6wBh1jaXjBu7'
-					className='bg-[#222222] border-2 rounded-full border-gamesRed h-12
+					className='bg-bgBlack border-2 rounded-full border-gamesRed h-12
 					justify-center items-center px-7 py-2.5 hidden lg:flex xl:w-1/2'
 				>
 					<p className='underline-animation font-oswald text-sm md:text-base text-white'>Become a Member</p>
--- client/src/shared/components/navigation-bar/components/NavBarLogo.jsx
+++ client/src/shared/components/navigation-bar/components/NavBarLogo.jsx
@@ -5,9 +5,13 @@ import { ReactComponent as NavLogoBig } from 'assets/gisau-logo/gisau.svg';
 // import { ReactComponent as NavLogoSmall } from 'assets/gisau-logo/gisau.svg';
 
 export default function NavBarLogo({ hasLandingImage }) {
+	const ScrollToTop = () => {
+		window.scrollTo(0, 0);
+	};
+
 	return (
 		<Box className='ml-6 lg:ml-16'>
-			<Link to='/'>
+			<Link to='/' onClick={ScrollToTop}>
 				{hasLandingImage ? (
 					<NavLogoBig className='w-14 sm:w-16 h-auto' />
 				) : (
