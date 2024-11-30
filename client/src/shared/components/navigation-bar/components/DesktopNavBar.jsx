63b86e7e7467155ed8d55716b820d67ba37ffaab
Matthew <mharyant@students.cs.ubc.ca>
2024-11-30 14:39:29 -0800

    GW-180: fix alignment

--- client/src/shared/components/navigation-bar/components/DesktopNavBar.jsx
+++ client/src/shared/components/navigation-bar/components/DesktopNavBar.jsx
@@ -10,17 +10,17 @@ export default function DesktopNavBar({ bgColor, hasLandingImage, pages, locatio
 	};
 
 	return (
-		<Box className='fixed w-full flex justify-between align-middle px-10 mt-8'>
+		<Box className='fixed w-full h-16 flex justify-between px-10 mt-8'>
 			<Box className='w-1/3'>
 				<NavBarLogo />
 			</Box>
 			<Box
-				className={`flex mx-6 navbar px-4 align-middle ${
+				className={`flex items-center mx-6 navbar px-4 ${
 					hasLandingImage ? bgColor : 'bg-white bg-opacity-70'
-				} h-14 rounded-full`}
+				} h-full rounded-full`}
 			>
 				{pages.map((page) => (
-					<Link key={page.name} to={page.path} className='px-5 pt-3' onClick={ScrollToTop} target='_self'>
+					<Link key={page.name} to={page.path} className='px-5' onClick={ScrollToTop} target='_self'>
 						<p
 							className={`underline-animation font-oswald text-md
 								${hasLandingImage ? 'text-white' : 'text-primary underline-animation-red'}
@@ -31,11 +31,11 @@ export default function DesktopNavBar({ bgColor, hasLandingImage, pages, locatio
 					</Link>
 				))}
 			</Box>
-			<Box className='w-1/3 flex justify-end'>
+			<Box className='w-1/3 flex justify-end items-center h-full'>
 				<Link
 					to='https://forms.gle/33ovq6wBh1jaXjBu7'
-					className='bg-bgBlack border-2 rounded-full border-gamesRed h-12
-					justify-center items-center px-7 py-2.5 hidden lg:flex xl:w-1/2'
+					className='bg-bgBlack border-2 rounded-full border-gamesRed h-fit
+					justify-center items-center px-3 md:px-7 py-2 md:py-2.5 hidden lg:flex xl:w-1/2'
 				>
 					<p className='underline-animation font-oswald text-sm md:text-base text-white'>Become a Member</p>
 				</Link>
