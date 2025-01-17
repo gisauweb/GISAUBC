2267672a0e00ef807eafb5cafacb96a9845daa65
gisauweb <gisauweb@gmail.com>
2025-01-17 15:27:24 -0800

    Add bakso malang rantangan

--- client/src/shared/data/upcoming_rantangan.js
+++ client/src/shared/data/upcoming_rantangan.js
@@ -1,10 +1,23 @@
-const UPCOMING_RANTANGAN = [];
+import { RANTANGAN_24_25 } from 'assets/rantangan-page/past-rantangan';
+import infoFile from 'assets/rantangan-page/info.pdf';
+
+const UPCOMING_RANTANGAN = [
+	{
+		image: RANTANGAN_24_25[0],
+		title: 'Bakso Malang',
+		description: 'Friday, January 24 | 12 - 4PM',
+		loc: 'UBC AMS Student Nest',
+		price: '$13 for GISAU members, $15 for non-members',
+		registrationLink: '',
+		infoLink: infoFile,
+	},
+];
 export default UPCOMING_RANTANGAN;
 
 // {
-//	image: rantangan,
-//	title: 'title',
-//	description: 'desc',
+// 	image: rantangan,
+// 	title: 'title',
+// 	description: 'desc',
 //  loc:'loc',
 //  price: '$10',
 //  registrationLink:'',
