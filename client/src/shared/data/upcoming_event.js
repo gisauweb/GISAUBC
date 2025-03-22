ed57a3b6d1be0ea83fff972781ecb9148696f3c5
msamanthaf <mariesamantha.f@gmail.com>
2025-03-21 18:38:55 -0700

    move core hiring to past events

--- client/src/shared/data/upcoming_event.js
+++ client/src/shared/data/upcoming_event.js
@@ -1,7 +1,5 @@
 import { EVENTS_24_25 } from 'assets/events-page/past-events';
-import infoFile from 'assets/events-page/AGM.pdf';
 import infoAgm from 'assets/events-page/infoAgm.pdf';
-import hiringPackage from 'assets/events-page/package.pdf';
 
 const UPCOMING_EVENTS = [
 	{
@@ -12,14 +10,6 @@ const UPCOMING_EVENTS = [
 		registrationLink:
 			'https://docs.google.com/forms/d/e/1FAIpQLSfVH0B4QYpNr-1NTpGMYJHyt8rpvb-sPkWXxKXu6fJcANBY9w/viewform',
 		infoLink: infoAgm,
-	},
-	{
-		image: EVENTS_24_25[8],
-		title: 'Core Hiring',
-		date: 'Wednesday, March 19 | 11:59PM',
-		loc: 'Register Via Link in Hiring Package',
-		registrationLink: hiringPackage,
-		infoLink: infoFile,
 	},
 ];
 
