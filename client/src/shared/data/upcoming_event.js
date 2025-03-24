e5b337dcd17dafe82f2bfdc97c7faff2efc072b9
msamanthaf <mariesamantha.f@gmail.com>
2025-03-23 20:32:27 -0700

    Add gala

--- client/src/shared/data/upcoming_event.js
+++ client/src/shared/data/upcoming_event.js
@@ -1,11 +1,20 @@
 import { EVENTS_24_25 } from 'assets/events-page/past-events';
 import infoAgm from 'assets/events-page/infoAgm.pdf';
+import gala from 'assets/events-page/gala.pdf'
 
 const UPCOMING_EVENTS = [
 	{
+		image: EVENTS_24_25[10],
+		title: 'GALA(LAND)',
+		date: 'Friday, April 4 | 5:00PM',
+		loc: 'AMS Nest Great Hall South',
+		registrationLink: 'https://forms.gle/79R2RPW78LPGTkW66',
+		infoLink: gala,
+	},
+	{
 		image: EVENTS_24_25[9],
 		title: 'Annual General Meeting',
-		date: 'Thursday, March 27 | 5:30PM - end',
+		date: 'Thursday, March 27 | 5:30PM',
 		loc: 'Asian Centre',
 		registrationLink:
 			'https://docs.google.com/forms/d/e/1FAIpQLSfVH0B4QYpNr-1NTpGMYJHyt8rpvb-sPkWXxKXu6fJcANBY9w/viewform',
