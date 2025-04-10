46f963917254ebd6b42f9464cfcbcf7b2736296f
msamanthaf <mariesamantha.f@gmail.com>
2025-04-09 23:33:49 -0700

    Display chattling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Popup from 'pages/pop-up/Popup';
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import { Box } from '@mui/material';
@@ -16,6 +16,20 @@ function App() {
 	};
 	const location = useLocation();
 	const shouldOpenPopup = ACTIVATE_POPUP && !isGamesPage(location.pathname);
+
+	useEffect(() => {
+		if (!ACTIVATE_POPUP && !document.getElementById('chatling-embed-script')) {
+			window.chtlConfig = { chatbotId: '1938486472' };
+
+			const script = document.createElement('script');
+			script.src = 'https://chatling.ai/js/embed.js';
+			script.async = true;
+			script.setAttribute('data-id', '1938486472');
+			script.id = 'chatling-embed-script';
+
+			document.body.appendChild(script);
+		}
+	}, [ACTIVATE_POPUP]);
 
 	return (
 		<Box className='bg-[#FFFDF5]'>
