51c9b3297f71b1cf7604cf84bb4824103728fa60
MatthewAnder <matthewanh@hotmail.com>
2025-05-28 23:31:56 -0700

    remove popup

--- client/src/shared/data/upcoming_event.js
+++ client/src/shared/data/upcoming_event.js
@@ -1,28 +1,28 @@
-import { EVENTS_24_25 } from 'assets/events-page';
-import { RANTANGAN_24_25 } from 'assets/rantangan-page';
+// import { EVENTS_24_25 } from 'assets/events-page';
+// import { RANTANGAN_24_25 } from 'assets/rantangan-page';
 // import infoFile from 'assets/rantangan-page/info.pdf';
 
 const UPCOMING_EVENTS = [
-	{
-		image: EVENTS_24_25[4],
-		title: 'Rumah Hantu',
-		date: 'October 27, 2024',
-		time: '3:00PM - 8:30PM',
-		loc: 'AMS Nest Room 2314',
-		registrationLink: 'https://forms.gle/rDxe3Ho5aJn9aBiN9',
-		isEvent: true,
-		// infoLink: ruhan,
-	},
-	{
-		image: RANTANGAN_24_25[0],
-		title: 'Rumah Hantu',
-		date: 'October 27, 2024',
-		time: '3:00PM - 8:30PM',
-		loc: 'AMS Nest Room 2314',
-		registrationLink: 'https://forms.gle/rDxe3Ho5aJn9aBiN9',
-		isEvent: false,
-		// infoLink: ruhan,
-	},
+	// {
+	// 	image: EVENTS_24_25[4],
+	// 	title: 'Rumah Hantu',
+	// 	date: 'October 27, 2024',
+	// 	time: '3:00PM - 8:30PM',
+	// 	loc: 'AMS Nest Room 2314',
+	// 	registrationLink: 'https://forms.gle/rDxe3Ho5aJn9aBiN9',
+	// 	isEvent: true,
+	// 	// infoLink: ruhan,
+	// },
+	// {
+	// 	image: RANTANGAN_24_25[0],
+	// 	title: 'Rumah Hantu',
+	// 	date: 'October 27, 2024',
+	// 	time: '3:00PM - 8:30PM',
+	// 	loc: 'AMS Nest Room 2314',
+	// 	registrationLink: 'https://forms.gle/rDxe3Ho5aJn9aBiN9',
+	// 	isEvent: false,
+	// 	// infoLink: ruhan,
+	// },
 ];
 
 export default UPCOMING_EVENTS;
