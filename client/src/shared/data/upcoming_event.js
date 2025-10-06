86799e6c37fe19139510435885d9a1c2232b3486
MatthewAnder <matthewanh@hotmail.com>
2025-10-05 17:34:35 -0700

    GW-211 chore: post fall hiring

--- client/src/shared/data/upcoming_event.js
+++ client/src/shared/data/upcoming_event.js
@@ -1,25 +1,34 @@
-// import { EVENTS_25_26 } from 'assets/events-page';
+import { EVENTS_25_26 } from 'assets/events-page';
 // import { RANTANGAN_24_25 } from 'assets/rantangan-page';
 // import infoFile from 'assets/rantangan-page/info.pdf';
 
 const UPCOMING_EVENTS = [
-	// 	{
-	// 		image: EVENTS_25_26[3],
-	// 		title: 'SOTO',
-	// 		caption: `Hey Thunderbirds... SOTO 2025 is FINALLY here! 🌟
-	// From fun games 🤸‍♀️, huge prizes 🏆, to a free Indonesian dinner 😋, SOTO is the perfect chance for you to meet new friends, reconnect with old ones, and learn more about what GISAU has planned for the year.
-	// 📍 AMS Nest Great Hall South
-	// 🕠 5:30PM – END
-	// 📅 Friday, Sept 26, 2025
-	// So make sure you invite your Sobat Tongkrongan(s)! and start your school years the right way — with community, culture, and lots of laughter. Sampai jumpa, we’ll see you there! 👋
-	// *This event is for GISAU Members only. Purchase your membership at the AMS Clubs Fair 💳`,
-	// 		// date: 'August 1th | 11:59PM PDT',
-	// 		loc: 'AMS Nest',
-	// 		registrationLink:
-	// 			'https://docs.google.com/forms/d/e/1FAIpQLScgRLJGYr_iI1t3470sgXFYJtNdneWHJXD0QfAjtJHms3Zpfw/viewform?usp=sharing&ouid=102355813688502067773',
-	// 		isEvent: true,
-	// 		// infoLink: '/past-events/package.pdf',
-	// 	},
+	{
+		image: EVENTS_25_26[4],
+		title: 'Fall Hiring',
+		caption: `We’re FALLing for YOU… because GISAU Fall Hiring is finally open! 🍁🫂
+
+Are you looking for a home away from home? 🏡
+Want to make new friends 🤝 and make memories with the BEST team ever? 📸
+
+If so... we want YOU in GISAU! 🇮🇩❤️
+
+We’re hiring for:
+🎉 Events Coordinator
+🎨 Creative Coordinator
+💻 UI/UX Designer
+🌐 Webmaster
+🍳 F&B Coordinator
+
+🗓️ Applications close Friday, October 10th, 2025 at 11:59PM PST!
+
+(For any hiring-related inquiries, DM us or email contact.gisau@gmail.com)`,
+		// date: 'August 1th | 11:59PM PDT',
+		loc: 'Online',
+		registrationLink: 'https://forms.gle/XTprZog16sn8g71J7',
+		isEvent: true,
+		infoLink: 'https://drive.google.com/file/d/1FJk_yMbl3MIIHhSYIQ92RRuAqKCA6up4/view?usp=sharing',
+	},
 ];
 
 export default UPCOMING_EVENTS;
