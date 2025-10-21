7492a5afce4b6d959b0e9156fb80aa87b85766cd
MatthewAnder <matthewanh@hotmail.com>
2025-10-21 13:55:13 -0700

    remove popup

--- client/src/shared/data/upcoming_event.js
+++ client/src/shared/data/upcoming_event.js
@@ -1,34 +1,29 @@
-import { EVENTS_25_26 } from 'assets/events-page';
+// import { EVENTS_25_26 } from 'assets/events-page';
 // import { RANTANGAN_24_25 } from 'assets/rantangan-page';
 // import infoFile from 'assets/rantangan-page/info.pdf';
 
 const UPCOMING_EVENTS = [
-	{
-		image: EVENTS_25_26[4],
-		title: 'Fall Hiring',
-		caption: `We’re FALLing for YOU… because GISAU Fall Hiring is finally open! 🍁🫂
-
-Are you looking for a home away from home? 🏡
-Want to make new friends 🤝 and make memories with the BEST team ever? 📸
-
-If so... we want YOU in GISAU! 🇮🇩❤️
-
-We’re hiring for:
-🎉 Events Coordinator
-🎨 Creative Coordinator
-💻 UI/UX Designer
-🌐 Webmaster
-🍳 F&B Coordinator
-
-🗓️ Applications close Friday, October 10th, 2025 at 11:59PM PDT!
-
-(For any hiring-related inquiries, DM us or email contact.gisau@gmail.com)`,
-		// date: 'August 1th | 11:59PM PDT',
-		loc: 'Online',
-		registrationLink: 'https://forms.gle/XTprZog16sn8g71J7',
-		isEvent: true,
-		infoLink: 'https://drive.google.com/file/d/1TDJwPMWMKcoTyrl4oGH3BR_KkBH3Sz7J/view?usp=sharing',
-	},
+	// {
+	// 		image: EVENTS_25_26[4],
+	// 		title: 'Fall Hiring',
+	// 		caption: `We’re FALLing for YOU… because GISAU Fall Hiring is finally open! 🍁🫂
+	// Are you looking for a home away from home? 🏡
+	// Want to make new friends 🤝 and make memories with the BEST team ever? 📸
+	// If so... we want YOU in GISAU! 🇮🇩❤️
+	// We’re hiring for:
+	// 🎉 Events Coordinator
+	// 🎨 Creative Coordinator
+	// 💻 UI/UX Designer
+	// 🌐 Webmaster
+	// 🍳 F&B Coordinator
+	// 🗓️ Applications close Friday, October 10th, 2025 at 11:59PM PDT!
+	// (For any hiring-related inquiries, DM us or email contact.gisau@gmail.com)`,
+	// 		// date: 'August 1th | 11:59PM PDT',
+	// 		loc: 'Online',
+	// 		registrationLink: 'https://forms.gle/XTprZog16sn8g71J7',
+	// 		isEvent: true,
+	// 		infoLink: 'https://drive.google.com/file/d/1TDJwPMWMKcoTyrl4oGH3BR_KkBH3Sz7J/view?usp=sharing',
+	// 	},
 ];
 
 export default UPCOMING_EVENTS;
