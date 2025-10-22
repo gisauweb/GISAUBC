d5a5cf6285cf0b1f8718e4309f62d6c6c381e915
MatthewAnder <matthewanh@hotmail.com>
2025-10-21 17:31:06 -0700

    upgrade tailwind

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	build: {
 		outDir: 'build', // CRA's default build output
 	},
