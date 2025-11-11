3e7291c79d3cf6f429aabc5b58c8b3925e4f6b30
MatthewAnder <matthewanh@hotmail.com>
2025-11-11 02:36:28 -0800

    remove listener

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,14 +2,7 @@ import express from "express";
 import post from "./routes/post";
 
 const app = express();
-const port = process.env.PORT || 8000;
 
 app.use("/post", post);
-
-if (process.env.NODE_ENV !== "production") {
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
-}
 
 export default app;
