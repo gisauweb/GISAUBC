c75be4d3e8eaaa637aad658725f62fb06cfcbf6a
MatthewAnder <matthewanh@hotmail.com>
2025-11-11 17:08:24 -0800

    fix function crashes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,19 @@
-import post from "./routes/post";
 import express from "express";
+import post from "./routes/post.js";
+import user from "./routes/user.js";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use("/posts", post);
+app.use("/users", user);
 
 app.get("/", (req, res) => {
   res.json({ message: "GISAU Backend API" });
 });
 
-app.use("/posts", post);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 export default app;
