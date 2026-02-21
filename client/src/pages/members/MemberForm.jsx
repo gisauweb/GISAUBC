56c0ca0229ffe7bb8ecaa1270cbac7eaf1116ab8
MatthewAnder <matthewanh@hotmail.com>
2026-02-21 11:03:05 -0800

    minor fix

--- client/src/pages/members/MemberForm.jsx
+++ client/src/pages/members/MemberForm.jsx
@@ -1,20 +1,10 @@
-import React, { useState } from 'react';
-import { useForm, useWatch } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
 import {
-	Box,
-	Typography,
-	TextField,
-	Radio,
-	RadioGroup,
-	FormControlLabel,
-	Checkbox,
-	Select,
-	MenuItem,
-	InputLabel,
-	FormControl,
+	Box
 } from '@mui/material';
 import GisauLogo from 'assets/gisau-logo/gisau_white.svg'; // Adjust path if needed
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 // --- Constants ---
 const FACULTIES = [
@@ -192,13 +182,6 @@ export default function MemberForm() {
 					<select
 						{...register('faculty', { required: true })}
 						className='w-full p-3 rounded-lg bg-gray-100 border-none focus:ring-2 focus:ring-[#732727] outline-none appearance-none'
-						style={{
-							backgroundImage:
-								'url("data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%22%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23000000%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E")',
-							backgroundRepeat: 'no-repeat',
-							backgroundPosition: 'right 1rem center',
-							backgroundSize: '0.80em',
-						}}
 					>
 						<option value='' disabled>
 							Choose One
@@ -216,13 +199,6 @@ export default function MemberForm() {
 					<select
 						{...register('year', { required: true })}
 						className='w-full p-3 rounded-lg bg-gray-100 border-none focus:ring-2 focus:ring-[#732727] outline-none appearance-none'
-						style={{
-							backgroundImage:
-								'url("data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%22%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23000000%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E")',
-							backgroundRepeat: 'no-repeat',
-							backgroundPosition: 'right 1rem center',
-							backgroundSize: '0.80em',
-						}}
 					>
 						<option value='' disabled>
 							Choose One
